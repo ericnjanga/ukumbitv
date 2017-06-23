680778601a8c01d9b04eaea4e2dc1912d23addb0
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-23 10:35:19 -0400

    styling login/registration layout

--- resources/views/layouts/user/focused.blade.php
+++ resources/views/layouts/user/focused.blade.php
@@ -39,6 +39,9 @@
     
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- background images slider -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
     <!--
     <script src="{{asset('streamtube/js/jquery.min.js')}}"></script>
     <script src="{{asset('streamtube/js/bootstrap.min.js')}}"></script>
