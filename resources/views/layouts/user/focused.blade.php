bc7e0e452f03db98a5f53f0e6d653759d11076c9
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-23 10:17:48 -0400

    styling login/registration layout

--- resources/views/layouts/user/focused.blade.php
+++ resources/views/layouts/user/focused.blade.php
@@ -37,6 +37,8 @@
 
     @include('layouts.user.footer')    
     
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!--
     <script src="{{asset('streamtube/js/jquery.min.js')}}"></script>
     <script src="{{asset('streamtube/js/bootstrap.min.js')}}"></script>
