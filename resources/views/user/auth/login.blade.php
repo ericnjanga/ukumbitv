26df69cd995a26fb1bfcbcb84af2b53f85fdec92
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-23 09:56:11 -0400

    styling login/registration layout

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="frame-login-registration" style="border:5px solid turquoise;">
+<div class="frame-login-registration">
     <div class="auth-rectangle">
 
          @include('notification.notify')
@@ -10,58 +10,9 @@
         <div class="social-form">
             <div class="signup-head">
                 <h3>Login</h3>
-            </div><!--end  of signup-head-->
-
-            <div class="social-btn">
-
-                @if(config('services.facebook.client_id') && config('services.facebook.client_secret'))
-                    <div class="social-fb">
-                        <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
-                            <input type="hidden" value="facebook" name="provider" id="provider">
-                            <a href="#">
-                                <button type="submit">
-                                    <i class="fa fa-facebook"></i>{{tr('login_via_fb')}}
-                                </button>
-                            </a>
-                        </form>
-                    </div>
-
-                @endif
-
-                @if(config('services.twitter.client_id') && config('services.twitter.client_secret'))
-
-                    <div class="social-twitter">
-                        <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
-                        <input type="hidden" value="twitter" name="provider" id="provider">
-                        <a href="#">
-                            <button type="submit">
-                                <i class="fa fa-twitter"></i>{{tr('login_via_twitter')}}
-                            </button>
-                        </a>
-                        </form>
-                    </div>
-
-                @endif
-
-                @if(config('services.google.client_id') && config('services.google.client_secret'))
-                    
-                    <div class="social-google">
-                        <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
-                            <input type="hidden" value="google" name="provider" id="provider">
-                            <a href="#">
-                                <button type="submit">
-                                    <i class="fa fa-google-plus"></i>{{tr('login_via_google')}}
-                                </button>
-                            </a>
-                        </form>
-                    </div>
-
-                @endif
-
-            </div><!--end of social-btn-->          
+            </div><!--end  of signup-head-->         
         </div><!--end of socila-form-->
-
-        <p class="col-xs-12 divider1">OR</p>
+ 
 
         <div class="sign-up login-page">
             {!! csrf_field() !!}
@@ -110,6 +61,25 @@
                 <p> <a href="{{ url('/password/reset') }}">{{tr('forgot_password')}}</a></p>           
             </form>
         </div><!--end of sign-up-->
+
+
+
+        
+
+            <div class="social-btn">
+                @if(config('services.facebook.client_id') && config('services.facebook.client_secret'))
+                    <div class="social-fb">
+                        <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
+                            <input type="hidden" value="facebook" name="provider" id="provider">
+                            <a href="#">
+                                <button type="submit">
+                                    <i class="fa fa-facebook"></i>{{tr('login_via_fb')}}
+                                </button>
+                            </a>
+                        </form>
+                    </div> 
+                @endif  
+            </div><!--end of social-btn--> 
 
     </div><!--end of common-form-->     
 </div><!--end of form-background-->
