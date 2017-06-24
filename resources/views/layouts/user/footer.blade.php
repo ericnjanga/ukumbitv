f0a66965f57f5ccebbba533be05e16b0cd9d9f1b
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-23 21:43:42 -0400

    Marking up footer

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -1,32 +1,44 @@
 <footer class="main-footer">
-    <div class="footer1 row">
-        <div class="col-sm-2 foot-div">
-            <div class="tube-image">
-                @if(Setting::get('site_logo'))
-                    <img src="{{Setting::get('site_logo')}}">
-                @else
-                    <img src="{{asset('logo.png')}}">
-                @endif
-            </div>                                 
-        </div><!--end of col-sm-2-->
+<!--   <div class="row">  
+    <div class="col-sm-2 foot-div">
+        <div class="tube-image">
+            @if(Setting::get('site_logo'))
+                <img src="{{Setting::get('site_logo')}}">
+            @else
+                <img src="{{asset('logo.png')}}">
+            @endif
+        </div>                                 
+    </div> 
 
-        <div class="col-sm-10 foot-content">
-            <!-- <ul class="about">
-                <li><a href="{{route('user.about')}}">About</a></li>
-                <li><a href="#">Copyright</a></li>
-                <li><a href="#">Press</a></li>
-                <li><a href="#">Creators</a></li>
-                <li><a href="#">Advertise</a></li>
-                <li><a href="#">Developers</a></li>
-                <li><a href="http://streamhash.com/" target="_blank">{{Setting::get('site_name' , 'StreamHash')}}</a></li>
-            </ul>
-         -->
-            <ul class="term">
-                <li><a href="{{route('user.about')}}">About Us</a></li>
-                <li><a href="{{route('user.terms-condition')}}">Terms &amp; Conditions</a></li>
-                <li><a href="{{route('user.privacy_policy')}}">Privacy &amp; Policy</a></li>
-                <li><a href="http://streamhash.com/" target="_blank">&#169; 2017 {{Setting::get('site_name' , 'StreamHash')}}</a></li>
-            </ul>
-        </div>   
-    </div><!--end of footer1-->
+    <div class="col-sm-10 foot-content">
+        
+        <ul class="term">
+            <li><a href="{{route('user.about')}}">About Us</a></li>
+            <li><a href="{{route('user.terms-condition')}}">Terms &amp; Conditions</a></li>
+            <li><a href="{{route('user.privacy_policy')}}">Privacy &amp; Policy</a></li>
+            <li><a href="http://streamhash.com/" target="_blank">&#169; 2017 {{Setting::get('site_name' , 'StreamHash')}}</a></li>
+        </ul>
+    </div>   
+  </div> --> 
+  <div class="row">
+  	<div class="col-md-12 text-center">
+  		<ul class="list-inline">
+  			<li>
+  				<a href="#">Terms of use</a>
+  			</li>
+  			<li>
+  				<a href="#">Privacy Statement</a>
+  			</li>
+  			<li>
+  				Questions? Contact us: <a href="mailto:info@ukumbitv.com">info@ukumbitv.com</a>
+  			</li>
+  		</ul>
+  	</div>
+
+  	<div class="col-md-12 text-center">
+  		<div>Copyright 2017, Toronto, Canada</div>
+			<div><a href="https://www.ukumbitv.com">ukumbitv</a></div>
+  	</div>
+  	 
+  </div> 
 </footer>
