43bb460d5fc45023bbae2843764eeeb9525d8255
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-25 11:24:10 -0400

    Change file directory

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,7 +8,7 @@
     <title>{{Setting::get('site_name' , 'Stream Hash')}}</title>
     <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/theme.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
 
     <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
