f5a551ac3bc1322398817e1e2bc50a459ea1d14a
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-26 06:11:00 -0400

    updating register template

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -9,130 +9,77 @@ page-registrater
 @endsection
 
 <div class="main-content">
-  <div class="rectangle panel-register frosting-glass">
-      
-      <div class="social-form">
-          <h3 class="title text-center">{{tr('signup')}}</h3>
+  <div class="rectangle panel-register frosting-glass"> 
+    <h3 class="page-title text-center">{{tr('signup')}}</h3>
+ 
+    <form class="signup-form" role="form" method="POST" action="{{ url('/register') }}">
 
-          <div class="social-btn">
+        {!! csrf_field() !!}
 
-              @if(config('services.facebook.client_id') && config('services.facebook.client_secret'))
-                  <div class="social-fb">
-                      <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
-                          <input type="hidden" value="facebook" name="provider" id="provider">
-                          <a href="#">
-                              <button type="submit">
-                                  <i class="fa fa-facebook"></i>{{tr('login_via_fb')}}
-                              </button>
-                          </a>
-                      </form>
-                  </div>
-              
-              @endif
+        @if($errors->has('email') || $errors->has('name') || $errors->has('password_confirmation') ||$errors->has('password'))
+            <div data-abide-error="" class="alert callout">
+                <p>
+                    <i class="fa fa-exclamation-triangle"></i> 
+                    <strong> 
+                        @if($errors->has('email')) 
+                            {{ $errors->first('email') }}
+                        @endif
 
-              @if(config('services.twitter.client_id') && config('services.twitter.client_secret'))
+                        @if($errors->has('name')) 
+                            {{ $errors->first('name') }}
+                        @endif
 
-                  <div class="social-twitter">
-                      <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
-                          <input type="hidden" value="twitter" name="provider" id="provider">
-                          <a href="#">
-                              <button type="submit">
-                                  <i class="fa fa-twitter"></i>{{tr('login_via_twitter')}}
-                              </button>
-                          </a>
-                      </form>
-                  </div>
+                        @if($errors->has('password')) 
+                            {{$errors->first('password')}}
+                        @endif
 
-              @endif
+                        @if($errors->has('password_confirmation'))
+                            {{ $errors->has('password_confirmation') }}
+                        @endif
 
-              @if(config('services.google.client_id') && config('services.google.client_secret'))
+                    </strong>
+                </p>
+            </div>
+        @endif
 
-                  <div class="social-google">
-                      <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
-                          <input type="hidden" value="google" name="provider" id="provider">
-                          <a href="#">
-                              <button type="submit">
-                                  <i class="fa fa-google-plus"></i>{{tr('login_via_google')}}
-                              </button>
-                          </a>
-                      </form>
-                  </div>
-                  
-              @endif
+        <div class="form-group">
+          <!-- <label for="name">{{tr('name')}}</label> -->
+          <input type="text" name="name" class="form-control" id="name" aria-describedby="emailHelp" placeholder="{{tr('name')}}">
+        </div>
 
-          </div><!--end of social-btn-->          
-      </div><!--end of socila-form-->
+        <div class="form-group">
+          <!-- <label for="email">{{tr('email')}}</label> -->
+          <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="{{tr('email')}}">
+        </div>
 
-      <p class="col-xs-12 divider1">OR</p>
+        <div class="form-group">
+          <!-- <label for="password">{{tr('password')}}</label> -->
+          <input type="password" name="password" class="form-control" id="password" placeholder="{{tr('password')}}">
+        </div>
 
-      <div class="sign-up">
+        <div class="form-group">
+          <!-- <label for="confirm_password">{{tr('confirm_password')}}</label> -->
+          <input type="password" name="password_confirmation" class="form-control" id="confirm_password" placeholder="{{tr('confirm_password')}}">
+        </div>
 
-          <form class="signup-form" role="form" method="POST" action="{{ url('/register') }}">
+        <button type="submit" class="btn btn-submit btn-block">{{tr('join_for_free_month')}}</button>
 
-              {!! csrf_field() !!}
+      	<!-- [Additional links] -->
+				<footer class="text-center">
+					<p>{{tr('already_account')}} <a href="{{route('user.login.form')}}">{{tr('login')}}</a></p> 
+				</footer> 
 
-              @if($errors->has('email') || $errors->has('name') || $errors->has('password_confirmation') ||$errors->has('password'))
-                  <div data-abide-error="" class="alert callout">
-                      <p>
-                          <i class="fa fa-exclamation-triangle"></i> 
-                          <strong> 
-                              @if($errors->has('email')) 
-                                  {{ $errors->first('email') }}
-                              @endif
-
-                              @if($errors->has('name')) 
-                                  {{ $errors->first('name') }}
-                              @endif
-
-                              @if($errors->has('password')) 
-                                  {{$errors->first('password')}}
-                              @endif
-
-                              @if($errors->has('password_confirmation'))
-                                  {{ $errors->has('password_confirmation') }}
-                              @endif
-
-                          </strong>
-                      </p>
-                  </div>
-              @endif
-
-              <div class="form-group">
-                  <label for="name">{{tr('name')}}</label>
-                  <input type="text" name="name" class="form-control" id="name" aria-describedby="emailHelp" placeholder="{{tr('name')}}">
-              </div>
-              <div class="form-group">
-                  <label for="email">{{tr('email')}}</label>
-                  <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="{{tr('email')}}">
-              </div>
-
-              <div class="form-group">
-                  <label for="password">{{tr('password')}}</label>
-                  <input type="password" name="password" class="form-control" id="password" placeholder="{{tr('password')}}">
-              </div>
-
-              <div class="form-group">
-                  <label for="confirm_password">{{tr('confirm_password')}}</label>
-                  <input type="password" name="password_confirmation" class="form-control" id="confirm_password" placeholder="{{tr('confirm_password')}}">
-              </div>
-
-              <input type="hidden" name="timezone" value="" id="userTimezone">
-
-              <div class="change-pwd">
-                  <button type="submit" class="btn btn-primary signup-submit">{{tr('join_for_free_month')}}</button>
-              </div>  
-              <p>{{tr('already_account')}} <a href="{{route('user.login.form')}}">{{tr('login')}}</a></p>         
-          </form>
-      </div><!--end of sign-up-->
+        <input type="hidden" name="timezone" value="" id="userTimezone">   
+    </form>  
   </div><!--end of common-form-->  
 
 
 
   <div class="rectangle panel-offers frosting-glass">
-  	<h2 class="page-title">
+  	<h3 class="page-title">
   		{{tr('you_get_per_month')}}:
-  	</h2>
-  	<ul>
+  	</h3>
+  	<ul class="list-unstyled">
   		<li class="text-uppercase">
   			[icon] Offer text
   		</li>
@@ -150,7 +97,7 @@ page-registrater
   		</li>
   	</ul>
 
-  	<h3 class="txt-color-primary">{{tr('first_month_free')}}!</h3>
+  	<h3 class="cta-title txt-color-primary">{{tr('first_month_free')}}!</h3>
   </div>   
 </div><!--form-background end-->
 
