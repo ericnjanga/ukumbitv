7c1ea1fdc9056754d33cb6e76bca448540b8dd17
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-26 22:50:29 -0400

    styles

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -66,7 +66,7 @@ page-register
 
       	<!-- [Additional links] -->
 				<footer class="text-center">
-					<p>{{trans('messages.already_account')}} <a href="{{route('user.login.form')}}">{{trans('messages.login')}}</a></p> 
+					<p>{{trans('messages.already_account')}} <a href="{{route('user.login.form')}}" class="underline">{{trans('messages.login')}}</a></p> 
 				</footer> 
 
         <input type="hidden" name="timezone" value="" id="userTimezone">   
