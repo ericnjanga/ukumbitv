6f2a46aeb2087a7df98435a3edc37b0a94b6477c
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-27 12:29:17 -0400

    edited welcome email template

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -6,109 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>{{Setting::get('site_name')}}</title>
     <style type="text/css">
-
         body {
             background-color: #282250;
-
-
 					/*Crisp text*/ 
 					-webkit-font-smoothing: antialiased;
 					-moz-osx-font-smoothing: grayscale;
         }
-
-/*        #outlook a {padding:0;}
-        body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
-        .ExternalClass {width:100%;}
-        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;}
-        #backgroundTable {margin:0; padding:0; width:100% !important; line-height: 100% !important;}
-        img {outline:none; text-decoration:none; -ms-interpolation-mode: bicubic;}
-        a img {border:none;display:inline-block;}
-        .image_fix {display:block;}
-
-        h1, h2, h3, h4, h5, h6 {color: white !important;}
-
-        h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {color: blue !important;}
-
-        h1 a:active, h2 a:active,  h3 a:active, h4 a:active, h5 a:active, h6 a:active {
-            color: red !important;
-        }
-
-        h1 a:visited, h2 a:visited,  h3 a:visited, h4 a:visited, h5 a:visited, h6 a:visited {
-            color: purple !important;
-        }
-
-        table td {border-collapse: collapse;}
-
-        table { border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
-
-        a {color: #000;}
-
-        @media only screen and (max-device-width: 480px) {
-
-            a[href^="tel"], a[href^="sms"] {
-                text-decoration: none;
-                color: black;  
-                pointer-events: none;
-                cursor: default;
-            }
-
-            .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
-                text-decoration: default;
-                color: orange !important;  
-                pointer-events: auto;
-                cursor: default;
-            }
-        }
-
-
-        @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
-            a[href^="tel"], a[href^="sms"] {
-                text-decoration: none;
-                color: blue; 
-                pointer-events: none;
-                cursor: default;
-            }
-
-            .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
-                text-decoration: default;
-                color: orange !important;
-                pointer-events: auto;
-                cursor: default;
-            }
-        }
-
-        p {
-            margin:0;
-            color:#FFF;
-            font-family:Helvetica, Arial, sans-serif;
-            font-size:16px;
-            line-height:160%;
-        }
-        a.link2{
-            text-decoration:none;
-            font-family:Helvetica, Arial, sans-serif;
-            font-size:16px;
-            color:#fff;
-            border-radius:4px;
-        }
-        h2{
-            color:#FFF;
-            font-family:Helvetica, Arial, sans-serif;
-            font-size:22px;
-            font-weight: normal;
-        }
-
-        .bgItem{
-            background:#cf4545;
-        }
-        .bgBody{
-            background:#ffffff;
-        }
-
-        .table-full{
-            height:100%;
-        }
-*/
     </style>
  
 
@@ -146,30 +49,30 @@
 								<tr>
 									<td align="center" valign="middle">
 										
-										<h3 style="margin-bottom:20px; font-size:18px; font-family: Roboto,sans-serif;">
+										<h3 style="margin-bottom:20px; font-size:18px; font-family: Roboto,sans-serif; color:#333;">
 			              	{{trans('messages.Welcome_to')}} <b>UkumbiTV</b> {{trans('messages.welcome_email_msg1')}}
 			              </h3>
 			              <h4 style=" font-size:16px;">For $2 per month, you get:</h4>
-			              <ul style="margin-bottom:20px; font-family: Roboto,sans-serif; font-size:16px;">
-			              	<li>item 1</li>
-			              	<li>item 2</li>
-			              	<li>item 3</li>
-			              	<li>item 4</li>
-			              	<li>item 5</li>
-			              	<li>item 6</li>
+			              <ul style="margin-bottom:20px;">
+			              	<li style="font-family: Roboto,sans-serif; font-size:16px; color:#333;">item 1</li>
+			              	<li style="font-family: Roboto,sans-serif; font-size:16px; color:#333;">item 2</li>
+			              	<li style="font-family: Roboto,sans-serif; font-size:16px; color:#333;">item 3</li>
+			              	<li style="font-family: Roboto,sans-serif; font-size:16px; color:#333;">item 4</li>
+			              	<li style="font-family: Roboto,sans-serif; font-size:16px; color:#333;">item 5</li>
+			              	<li style="font-family: Roboto,sans-serif; font-size:16px; color:#333;">item 6</li>
 			              </ul>
-			              <p style="font-family: Roboto,sans-serif; font-size:16px;">{{trans('messages.welcome_email_msg2')}}</p>
+			              <p style="font-family: Roboto,sans-serif; font-size:16px; color:#333;">{{trans('messages.welcome_email_msg2')}}</p>
 									</td>
 								</tr>
 								<tr>
-									<td height="50"></td>
+									<td height="25"></td>
 								</tr>
 								<tr>
-									<td align="center" style="padding-top:25px;padding-bottom:115px;"> 
+									<td align="center"> 
 			              <table cellpadding="0" cellspacing="0" border="0" cellpadding="0" align="center" width="300">
 
 			                <tr>
-			                  <td bgcolor="#ec174f" align="center" style="border-radius:4px;margin-top: 10px" width="200" height="50">
+			                  <td bgcolor="#ec174f" align="center" style="border-radius:4px;" width="200" height="50">
 			                     
 			                    <a target='_blank' href="{{route('email.verify' , ['id' => $email_data->id , 'verification_code' => $email_data->verification_code])}}" style="color: #fff; font-family: Roboto,sans-serif; display: block; padding: 15px; text-decoration: none; font-size: 16px; text-transform: uppercase;">{{trans('messages.Verify_Now')}}</a>
 			                       
@@ -179,7 +82,7 @@
 			            </td> 
 								</tr>
 								<tr>
-									<td height="50"></td>
+									<td height="25"></td>
 								</tr>
 							</table>
 							
@@ -191,10 +94,16 @@
  
 
  					<tr>
- 						<td height="150" align="center" valign="middle"> 
-							<p style="color: #fff; font-size:9px; color:#ccc; font-family: Roboto,sans-serif; text-align-center;">This email was sent to you because you indicated that you'd like to receive updates about new features and offers to improve your listing's performance from Google My Business. If you don't want to receive such emails in the future, please unsubscribe here. You can also change your preferences on your Google My Business settings page by logging into https://business.google.com/settings. </p>
- 							<p style="color: #fff; font-size:9px; color:#ccc; font-family: Roboto,sans-serif; text-align-center;">Pleace website footer links</p>
- 							<p style="color: #fff; font-size:9px; color:#ccc; font-family: Roboto,sans-serif; text-align-center;">Pleace the eastmall address</p>
+ 						<td height="250" align="center" valign="middle"> 
+ 							<table cellpadding="0" cellspacing="0" border="0" cellpadding="0" align="center" width="400">
+								<tr>
+									<td> 
+										<p style="font-size:9px; color:#ccc; font-family: Roboto,sans-serif; text-align:center;">This email was sent to you because you indicated that you'd like to receive updates about new features and offers to improve your listing's performance from Google My Business. If you don't want to receive such emails in the future, please unsubscribe here. You can also change your preferences on your Google My Business settings page by logging into https://business.google.com/settings. </p>
+			 							<p style="font-size:9px; color:#ccc; font-family: Roboto,sans-serif; text-align:center;">Pleace website footer links</p>
+			 							<p style="font-size:9px; color:#ccc; font-family: Roboto,sans-serif; text-align:center;">Pleace the eastmall address</p>
+									</td>
+								</tr>
+ 							</table>
  						</td>
  					</tr>
 				</table>
