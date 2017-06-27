cca6bcddf2bcf9ae90d5c1b1c2018edbc20edccc
Alexey Tregubov <Alexey Tregubov>
2017-06-27 20:11:58 +0300

    alert fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,9 +28,9 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
+            \App\Http\Middleware\Locale::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyUser::class,
-            \App\Http\Middleware\Locale::class,
             // \App\Http\Middleware\VerifyCsrfToken::class,
         ],
 
