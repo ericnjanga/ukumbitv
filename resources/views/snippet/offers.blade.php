e846546ef7327c3a2baf508fc94450ff90dc5466
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-29 07:59:31 -0400

    edited template

--- resources/views/snippet/offers.blade.php
+++ resources/views/snippet/offers.blade.php
@@ -1,7 +1,7 @@
 <ul class="list-unstyled">
     @foreach (trans('offers') as $offer)
         <li class="text-uppercase">
-            @include('svg-like1') {{$offer}}
+            @include('snippet.svg-like1') {{$offer}}
         </li>
     @endforeach
 </ul>
