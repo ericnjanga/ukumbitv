7606061169ba3cf2bff304efebf42ab273bb3113
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-30 19:28:48 -0400

    testing video thumbnail

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -22,7 +22,42 @@
     	@include('layouts.user.latest-uploads.hero-carousel')
 
 
-    	<div class="col-sm-6 col-md-4">
+			
+    	<div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="http://via.placeholder.com/300x250" alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>...</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+		      </div>
+		    </div>
+		  </div> 
+			
+    	<div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="http://via.placeholder.com/300x250" alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>...</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+		      </div>
+		    </div>
+		  </div>
+
+			
+    	<div class="col-sm-6 col-md-3">
+		    <div class="thumbnail">
+		      <img src="http://via.placeholder.com/300x250" alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>...</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+		      </div>
+		    </div>
+		  </div> 
+			
+    	<div class="col-sm-6 col-md-3">
 		    <div class="thumbnail">
 		      <img src="http://via.placeholder.com/300x250" alt="...">
 		      <div class="caption">
