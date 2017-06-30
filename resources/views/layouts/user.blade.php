f8119257df15bea8c461d132ba3dd320409765e7
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-30 19:50:00 -0400

    testing video thumbnail

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -18,55 +18,57 @@
 	<body class="page-videos"> 
     @include('layouts.user.header')
 
-    <div class="main-content">
+    <div class="main-content video-content">
     	@include('layouts.user.latest-uploads.hero-carousel')
 
 
-			
-    	<div class="col-sm-6 col-md-3">
-		    <div class="thumbnail">
-		      <img src="http://via.placeholder.com/300x250" alt="...">
-		      <div class="caption">
-		        <h3>Thumbnail label</h3>
-		        <p>...</p>
-		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-		      </div>
-		    </div>
-		  </div> 
-			
-    	<div class="col-sm-6 col-md-3">
-		    <div class="thumbnail">
-		      <img src="http://via.placeholder.com/300x250" alt="...">
-		      <div class="caption">
-		        <h3>Thumbnail label</h3>
-		        <p>...</p>
-		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-		      </div>
-		    </div>
-		  </div>
+			<div class="row">
+	    	<div class="col-sm-6 col-md-3">
+			    <div class="thumbnail">
+			      <img src="http://via.placeholder.com/300x250" alt="...">
+			      <div class="caption">
+			        <h3>Thumbnail label</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			      </div>
+			    </div>
+			  </div> 
+				
+	    	<div class="col-sm-6 col-md-3">
+			    <div class="thumbnail">
+			      <img src="http://via.placeholder.com/300x250" alt="...">
+			      <div class="caption">
+			        <h3>Thumbnail label</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			      </div>
+			    </div>
+			  </div>
 
-			
-    	<div class="col-sm-6 col-md-3">
-		    <div class="thumbnail">
-		      <img src="http://via.placeholder.com/300x250" alt="...">
-		      <div class="caption">
-		        <h3>Thumbnail label</h3>
-		        <p>...</p>
-		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-		      </div>
-		    </div>
-		  </div> 
-			
-    	<div class="col-sm-6 col-md-3">
-		    <div class="thumbnail">
-		      <img src="http://via.placeholder.com/300x250" alt="...">
-		      <div class="caption">
-		        <h3>Thumbnail label</h3>
-		        <p>...</p>
-		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-		      </div>
-		    </div>
-		  </div>
+				
+	    	<div class="col-sm-6 col-md-3">
+			    <div class="thumbnail">
+			      <img src="http://via.placeholder.com/300x250" alt="...">
+			      <div class="caption">
+			        <h3>Thumbnail label</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			      </div>
+			    </div>
+			  </div> 
+				
+	    	<div class="col-sm-6 col-md-3">
+			    <div class="thumbnail">
+			      <img src="http://via.placeholder.com/300x250" alt="...">
+			      <div class="caption">
+			        <h3>Thumbnail label</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			      </div>
+			    </div>
+			  </div>
+					
+			</div>
 
 
     	<!-- 
