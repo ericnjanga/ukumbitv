bc0e45e6ec9372af83af3b5dff3e874b7ec2cee4
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-06-30 19:53:01 -0400

    testing video thumbnail

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -28,8 +28,7 @@
 			      <img src="http://via.placeholder.com/300x250" alt="...">
 			      <div class="caption">
 			        <h3>Thumbnail label</h3>
-			        <p>...</p>
-			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			        <p>...</p> 
 			      </div>
 			    </div>
 			  </div> 
@@ -39,34 +38,30 @@
 			      <img src="http://via.placeholder.com/300x250" alt="...">
 			      <div class="caption">
 			        <h3>Thumbnail label</h3>
-			        <p>...</p>
-			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			        <p>...</p> 
 			      </div>
 			    </div>
-			  </div>
+			  </div> 
 
-				
 	    	<div class="col-sm-6 col-md-3">
 			    <div class="thumbnail">
 			      <img src="http://via.placeholder.com/300x250" alt="...">
 			      <div class="caption">
 			        <h3>Thumbnail label</h3>
-			        <p>...</p>
-			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			        <p>...</p> 
 			      </div>
 			    </div>
 			  </div> 
-				
+			  
 	    	<div class="col-sm-6 col-md-3">
 			    <div class="thumbnail">
 			      <img src="http://via.placeholder.com/300x250" alt="...">
 			      <div class="caption">
 			        <h3>Thumbnail label</h3>
-			        <p>...</p>
-			        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+			        <p>...</p> 
 			      </div>
 			    </div>
-			  </div>
+			  </div> 
 					
 			</div>
 
