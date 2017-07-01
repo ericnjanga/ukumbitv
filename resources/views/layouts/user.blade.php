b0f97842c1d7f5a4a53b69c16e8e68b76a04cc85
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-01 09:01:36 -0400

    building video thumbnail

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -40,7 +40,7 @@
 								<div class="panel-group thumbnail-info2" id="accordion" role="tablist" aria-multiselectable="true">
 								  <div class="panel panel-default">
 								    <div class="panel-heading" role="tab" id="headingOne">
-								      <h4 class="panel-title">
+								      <h4 class="panel-title--">
 								        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
 								          Collapsible Group Item #1
 								        </a>
