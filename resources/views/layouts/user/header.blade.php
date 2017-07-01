b5adeb83dd18c26c9918ad3db4af5bacfc60bea5
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-01 17:21:13 -0400

    building main navigation

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -1,11 +1,11 @@
-<div class="main-nav">
+<!-- <div class="main-nav">
 	<div class="container">
 		<div class="col-md-12">
 	    <div class="brand pull-left">
 	      <a href="{{route('user.dashboard')}}" alt="UKUMBITV" class="y-image">
 	      	<img src="{{asset('streamtube/images/logo1.png')}}" alt='UkumbiTv' />
 	      </a>
-	    </div><!--test end-->
+	    </div>
 
 	    <div class="main-nav__additional-links pull-right">
 
@@ -24,79 +24,76 @@
         <div class="dropdown">
           <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
           	fklfenwf fwekfew
-    				<span class="caret"></span>
-            <!-- @if(Auth::user()->picture != "")
-                <img class="profile-image" src="{{Auth::user()->picture}}">
-            @else
-                <img class="profile-image" src="{{asset('placeholder.png')}}">
-            @endif -->
+    				<span class="caret"></span> 
           </button>
           <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
             <li><a href="{{route('user.profile')}}">{{tr('account')}}</a></li> 
             <li role="separator" class="divider"></li>
             <li><a href="{{route('user.logout')}}">{{tr('logout')}}</a></li>
-            <!-- <li><a href="{{route('user.history')}}">{{tr('history')}}</a></li> 
-            <li><a href="{{route('user.spam-videos')}}">{{tr('spam_videos')}}</a></li>
-             <li><a href="{{route('user.pay-per-videos')}}">{{tr('pay_per_videos')}}</a></li>
-            @if (Auth::user()->login_by == 'manual') 
-                <li role="separator" class="divider"></li>
-                <li><a href="{{route('user.change.password')}}">{{tr('change_password')}}</a></li>
-            @endif
-
-            <li role="separator" class="divider"></li>
-            <li><a href="{{route('user.delete.account')}}" @if(Auth::user()->login_by != 'manual') onclick="return confirm('Are you sure? . Once you deleted account, you will lose your history and wishlist details.')" @endif>{{tr('delete_account')}}</a></li>
-            -->
           </ul>
         </div>
-      </div>
-
-
-
-
-
+      </div> 
 
 	  </div>
 	</div> 
-</div>
-
-
-
-
-<!-- <div class="youtube-nav">
-    <div class="row">
-        <div class="test toggle1">
-            <a href="#"><i class="fa fa-align-justify toggle-icon" aria-hidden="true"></i></a>
-        </div>
-        <div class="test you-image">
-            <a href="{{route('user.dashboard')}}" class="y-image">
-                @if(Setting::get('site_logo'))
-                    <img src="{{Setting::get('site_logo')}}">
-                @else
-                    <img src="{{asset('logo.png')}}">
-                @endif
-            </a>
-        </div>
-
-        <div id="custom-search-input" class="">
-            <form method="post" action="{{route('search-all')}}" id="userSearch">
-            <div class="input-group">
-                
-                    <input type="text" id="auto_complete_search" name="key" class="search-query form-control" required placeholder="Search" />
-                    <span class="input-group-btn">
-                        <button class="btn btn-danger" type="submit">
-                        <span class=" glyphicon glyphicon-search"></span>
-                        </button>
-                    </span>
-                
-            </div>
-            </form>
-        </div>
-
-        @if(Auth::check())
-         
-        @endif
-
-    </div>
 </div> -->
+
+
+
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
 
 
