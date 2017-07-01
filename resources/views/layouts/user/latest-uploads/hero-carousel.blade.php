3d21f9d32fa647bb8598c9f1ffe3dff55defb6b5
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-01 11:12:06 -0400

    building video thumbnail

--- resources/views/layouts/user/latest-uploads/hero-carousel.blade.php
+++ resources/views/layouts/user/latest-uploads/hero-carousel.blade.php
@@ -1,4 +1,4 @@
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="carousel-example-generic" class="carousel slide billboard" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -9,21 +9,27 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="https://ukumbitv.com/streamtube/images/deco/car-x9fwkuz94596.jpg" alt="...">
+    	<img src="http://via.placeholder.com/1200x650">
+      <!-- <img src="https://ukumbitv.com/streamtube/images/deco/car-x9fwkuz94596.jpg" alt="..."> -->
       <div class="carousel-caption">
-        ...
+      	<h3>Sweet roll toffee biscuit</h3>
+        <p>Sweet roll ice cream toffee pudding sesame snaps. Jujubes brownie ice cream tiramisu cookie. Sweet roll toffee biscuit lollipop powder donut danish.</p>
       </div>
     </div>
     <div class="item">
-      <img src="https://ukumbitv.com/streamtube/images/deco/car-x9fwkuz96701.jpg" alt="...">
+    	<img src="http://via.placeholder.com/1200x650">
+      <!-- <img src="https://ukumbitv.com/streamtube/images/deco/car-x9fwkuz96701.jpg" alt="..."> -->
       <div class="carousel-caption">
-        ...
+      	<h3>Gummies candy canes</h3>
+        <p>Jelly beans tiramisu chocolate. Gummies candy canes ice cream candy canes biscuit croissant soufflé. Cotton candy biscuit lemon drops. Cupcake tootsie roll gingerbread pie</p>
       </div>
     </div>
     <div class="item">
-      <img src="https://ukumbitv.com/streamtube/images/deco/car-x9fwkuz96805.jpg" alt="...">
+    	<img src="http://via.placeholder.com/1200x650">
+      <!-- <img src="https://ukumbitv.com/streamtube/images/deco/car-x9fwkuz96805.jpg" alt="..."> -->
       <div class="carousel-caption">
-        ...
+      	<h3>muffin cotton candy</h3>
+        <p>Halvah bonbon toffee donut muffin cotton candy donut. Sweet roll caramels icing caramels lollipop bonbon sweet roll bonbon candy. Dragée biscuit icing tootsie roll brownie dessert. Chupa chups sugar plum sweet cupcake brownie marzipan powder pie.</p>
       </div>
     </div>
   </div>
