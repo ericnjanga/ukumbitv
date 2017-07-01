d046cb95bd9809279ee15fc5445c4af203e3ff0d
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-01 17:23:56 -0400

    building main navigation

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -38,10 +38,10 @@
 	</div> 
 </div> -->
 
+    
 
 
-
-<nav class="navbar navbar-default">
+<nav class="main-nav navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
