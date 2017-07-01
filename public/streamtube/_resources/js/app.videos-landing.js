4a5b3d687be0b67da0abccbe7f7bf14b2177bbcd
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-01 10:19:06 -0400

    building video thumbnail

--- public/streamtube/_resources/js/app.videos-landing.js
+++ public/streamtube/_resources/js/app.videos-landing.js
@@ -5,8 +5,10 @@
 	//some code
 	$('body').on('mouseenter', '.video-thumbnail', function(){
 		$(this).find('.video-thumbnail-accordion').collapse('show');
+		console.log('>>>> mouseenter');
 	}).on('mouseenter', '.video-thumbnail', function(){
 		$(this).find('.video-thumbnail-accordion').collapse('hide');
+		console.log('>>>> mouseenter');
 	});
 
 })(jQuery);
