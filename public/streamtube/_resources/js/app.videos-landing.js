61c394b7d5d72ce5345c9ffae9e30edf9bf9a66f
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-02 09:51:59 -0400

    Setting up grid system

--- public/streamtube/_resources/js/app.videos-landing.js
+++ public/streamtube/_resources/js/app.videos-landing.js
@@ -5,6 +5,8 @@
 
 //Initiate the grid system (only on the video page)
 $(function() {
+	console.log('>>>>>', $('.page-videos').length);
+	console.log('>>>>>Grid=', Grid);
 	if($('.page-videos').length > 0){
 		Grid.init();
 	}
