d4f11ae0e1ccb486904bed2c7137c0cf453441b8
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-02 10:25:11 -0400

    Setting up grid system

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -444,13 +444,15 @@
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script> 
  		
- 		<script src="{{asset('streamtube/_resources/js/grid.js')}}"></script>
- 		
-		<script>
+ 		<!--<script src="{{asset('streamtube/_resources/js/grid.js')}}"></script>-->
+ 		<script src="{{asset('streamtube/js/grid.js')}}"></script>
+ 		<script src="{{asset('streamtube/js/app.videos-landing.js')}}"></script>
+
+		<!--<script>
 			$(function() {
 				Grid.init();
 			});
-		</script>
+		</script>-->
   	<!-- Video Page script -->
 	  <!--<script src="{{asset('streamtube/js/app.videos-landing.js')}}"></script>-->
     @yield('scripts') 
