2a36e4d1a752325c29e22eeb651cc08c645dd023
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-05 20:50:45 -0400

    improving single video page

--- resources/views/user/single_newvideo.blade.php
+++ resources/views/user/single_newvideo.blade.php
@@ -17,26 +17,26 @@
 
 @section('content')
 
-<div id="player"></div>
+<div id="player">{{$video->video}}</div>
 <script type="text/javascript">
-    var conf = {
-        key:       "bb175f20-6e3a-4edb-af12-619f8e67c88e",
-        autoplay : true,
-        source: {
-          dash:        "{{$video->video}}",
-          hls:         "{{$video->video}}",
-          progressive: "{{$video->video}}",
-          poster:      "//bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg"
-        }
-    };
-    var player = bitmovin.player("player");
-    player.setup(conf).then(function(value) {
-        // Success
-        console.log("Successfully created bitmovin player instance");
-    }, function(reason) {
-        // Error!
-        console.log("Error while creating bitmovin player instance");
-    });
+    // var conf = {
+    //     key:       "bb175f20-6e3a-4edb-af12-619f8e67c88e",
+    //     autoplay : true,
+    //     source: {
+    //       dash:        "{{$video->video}}",
+    //       hls:         "{{$video->video}}",
+    //       progressive: "{{$video->video}}",
+    //       poster:      "//bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg"
+    //     }
+    // };
+    // var player = bitmovin.player("player");
+    // player.setup(conf).then(function(value) {
+    //     // Success
+    //     console.log("Successfully created bitmovin player instance");
+    // }, function(reason) {
+    //     // Error!
+    //     console.log("Error while creating bitmovin player instance");
+    // });
 </script>
 
 
