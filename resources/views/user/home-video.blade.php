ec03a1c44aa465db266c24223e2b0fceabd257db
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-08 07:37:06 -0400

    (videos) replacing placeholder image by real one

--- resources/views/user/home-video.blade.php
+++ resources/views/user/home-video.blade.php
@@ -19,12 +19,14 @@ page-homevideos
       <ul id="og-grid" class="og-grid video-content list-unstyled clearfix">
         @foreach($videos as $video)
           <li class="col">
-          	{{$video}}
+          	{{$video->$images}}
               <!--
-              {"id":2,"title":"Sample Video","description":"Sample Video","category_id":"1","sub_category_id":"1","genre_id":"0","video":"https:\/\/ukumbitv.com\/uploads\/videos\/original\/994420650bd0f400f90b365b3e2d25355779f677.mp4","trailer_video":"https:\/\/ukumbitv.com\/uploads\/videos\/original\/90be4aa8f7d92fce852d6377c33d3ae3880d5b20.mp4","default_image":"https:\/\/ukumbitv.com\/uploads\/images\/bcbe22984f787736e440b086828dda8024595d69.jpg","banner_image":"","ratings":"5","reviews":"Sample Video","status":"1","is_approved":"1","is_home_slider":"0","is_banner":"0","uploaded_by":"admin","publish_time":"2017-06-01 00:00:00","duration":"00:10:00","trailer_duration":"00:00:00","video_resolutions":"","trailer_video_resolutions":"","compress_status":"1","trailer_compress_status":"1","video_resize_path":null,"trailer_resize_path":null,"edited_by":"admin","watch_count":"1","type_of_user":"0","type_of_subscription":"0","amount":"0","created_at":"2017-06-01 14:01:57","updated_at":"2017-06-01 14:05:11","video_type":"1","video_upload_type":"2","actors":null,"directors":null,"watchid":"201707030106451"}
+              
+          	{"id":14,"title":"edited title","description":"111111","category_id":"1","sub_category_id":"1","genre_id":"0","video":"https:\/\/ukumbitv.com\/movies\/20170707121053\/1499429453321.mp4","trailer_video":"trailer url","default_image":"https:\/\/ukumbitv.com\/images\/20170707121053\/billboard1499429453bil.png","banner_image":"","ratings":"5","reviews":"review","status":"1","is_approved":"1","is_home_slider":"0","is_banner":"0","uploaded_by":"admin","publish_time":"2017-06-05 10:45:00","duration":"11:11:11","trailer_duration":"00:00:00","video_resolutions":null,"trailer_video_resolutions":null,"compress_status":"0","trailer_compress_status":"0","video_resize_path":null,"trailer_resize_path":null,"edited_by":"admin","watch_count":"1","type_of_user":"0","type_of_subscription":"0","amount":"0","created_at":"2017-07-07 12:10:53","updated_at":"2017-07-08 10:42:00","video_type":"1","video_upload_type":"2","actors":"1,2","directors":"1,2","watchid":"20170707121053","lang_id":"1","year":"2015"}
+              
           -->
             <a href="{{url('/')}}/watch/{{$video->watchid}}" data-largesrc="{{$video->imgSmall1}}" data-title="{{$video->title}}" data-description="{{$video->description}}">
-                <img class="og-tmb1" src="https://via.placeholder.com/250x170" alt="{{$video->title}}"/>
+                <img class="og-tmb1" src="{{$video->imgSmall1}}" alt="{{$video->title}}"/>
             </a> 
           </li>
         @endforeach 
