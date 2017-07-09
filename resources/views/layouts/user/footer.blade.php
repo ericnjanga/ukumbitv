008a79caf2ee7d6a9ad0a06787f056be27025080
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-09 10:37:41 -0400

    Restricted "about" and "legal" pages for logged in users only

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -2,7 +2,11 @@
 	<div class="container">
 	  <div class="row">
 	  	<div class="col-md-12 text-center">
-	  		<ul class="list-inline">
+	  		
+
+				{{-- ONLY FOR LOGGED IN USERS --}}
+	  		@if (Auth::check())
+				<ul class="list-inline">
 	  			<li>
 	  				<a href="{{url('/about-us')}}" class="underlined">{{trans('messages.about_us')}}</a>
 	  			</li> 
@@ -16,6 +20,16 @@
 	  				<a href="{{url('/jobs')}}" class="underlined">{{trans('messages.jobs')}}</a>
 	  			</li> 
 	  		</ul>
+				@endif
+				{{-- ONLY FOR LOGGED IN USERS --}}
+
+
+
+
+
+
+
+
 	  		<ul class="list-inline"> 
 	  			<li>
 	  				{{trans('messages.question_contactus')}}: <a href="mailto:info@ukumbitv.com" class="underlined">info@ukumbitv.com</a>
