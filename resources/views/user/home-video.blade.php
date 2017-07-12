b1cf7fa8e1b0770da7b55f573f572975122ad173
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-12 10:21:56 -0400

    add lazy loading

--- resources/views/user/home-video.blade.php
+++ resources/views/user/home-video.blade.php
@@ -29,8 +29,11 @@ page-homevideos
           -->
             <a href="#" data-largesrc="{{$video->videoimage->imgPreview}}" data-title="{{$video->title}}" data-description="{{$video->description}}" data-theyear="{{$video->year}}" data-duration="{{$video->duration}}"  data-toggle="modal" data-target="#videoModal">
 
-	    					<!--<img class="img-loading" src="{{asset('streamtube/images/loader.gif')}}"/>-->
-                <img class="og-tmb1 lazy" data-src="{{$video->videoimage->imgSmall1}}" src="http://via.placeholder.com/500x340" alt="{{$video->title}}"/>
+	    					<!--<img class="img-loading" src="{{asset('streamtube/images/loader.gif')}}"/>
+
+src="http://via.placeholder.com/500x340"
+	    				-->
+                <img class="og-tmb1 lazy" src="{{$video->videoimage->imgSmall1}}"  alt="{{$video->title}}"/>
 
                  
             </a> 
@@ -96,88 +99,5 @@ page-homevideos
  	<script src="{{asset('streamtube/js/grid.js')}}"></script>-->
   <script src="{{asset('streamtube/js/app.home-videos.js')}}"></script>
 
-
-  <script>
-  /* lazyload.js (c) Lorenzo Giuliani
- * MIT License (http://www.opensource.org/licenses/mit-license.html)
- *
- * expects a list of:  
- * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
- */
-
-!function(window){
-  var $q = function(q, res){
-        if (document.querySelectorAll) {
-          res = document.querySelectorAll(q);
-        } else {
-          var d=document
-            , a=d.styleSheets[0] || d.createStyleSheet();
-          a.addRule(q,'f:b');
-          for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
-            l[b].currentStyle.f && c.push(l[b]);
-
-          a.removeRule(0);
-          res = c;
-        }
-        return res;
-      }
-    , addEventListener = function(evt, fn){
-        window.addEventListener
-          ? this.addEventListener(evt, fn, false)
-          : (window.attachEvent)
-            ? this.attachEvent('on' + evt, fn)
-            : this['on' + evt] = fn;
-      }
-    , _has = function(obj, key) {
-        return Object.prototype.hasOwnProperty.call(obj, key);
-      }
-    ;
-
-  function loadImage (el, fn) {
-    var img = new Image()
-      , src = el.getAttribute('data-src');
-    img.onload = function() {
-      if (!! el.parent)
-        el.parent.replaceChild(img, el)
-      else
-        el.src = src;
-
-      fn? fn() : null;
-    }
-    img.src = src;
-  }
-
-  function elementInViewport(el) {
-    var rect = el.getBoundingClientRect()
-
-    return (
-       rect.top    >= 0
-    && rect.left   >= 0
-    && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-    )
-  }
-
-    var images = new Array()
-      , query = $q('img.lazy')
-      , processScroll = function(){
-          for (var i = 0; i < images.length; i++) {
-            if (elementInViewport(images[i])) {
-              loadImage(images[i], function () {
-                images.splice(i, i);
-              });
-            }
-          };
-        }
-      ;
-    // Array.prototype.slice.call is not callable under our lovely IE8 
-    for (var i = 0; i < query.length; i++) {
-      images.push(query[i]);
-    };
-
-    processScroll();
-    addEventListener('scroll',processScroll);
-
-}(this);​
-  </script>
 @endsection
  
