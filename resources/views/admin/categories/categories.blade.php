0dfb072a71bea15310052c82b24bd2aa26c05a6f
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-20 11:42:35 -0400

    Styling admin login

--- resources/views/admin/categories/categories.blade.php
+++ resources/views/admin/categories/categories.blade.php
@@ -20,39 +20,37 @@
                 <b style="font-size:18px;">{{tr('categories')}}</b>
                 {{--<a href="{{route('admin.add.category')}}" class="btn btn-default pull-right">{{tr('add_category')}}</a>--}}
             </div> -->
-            <div class="row">
+            
 
-            	@if(count($categories) > 0)
+          	@if(count($categories) > 0) 
+	            <table id="example1" class="table table-bordered table-striped">
+								<thead>
+								    <tr>
+								      <th>{{tr('id')}}</th>
+								      <th>{{tr('category')}}</th>
+								      <th>{{tr('action')}}</th>
+								    </tr>
+								</thead>
 
-	              	<table id="example1" class="table table-bordered table-striped">
+								<tbody>
+									@foreach($categories as $i => $category)
 
-						<thead>
-						    <tr>
-						      <th>{{tr('id')}}</th>
-						      <th>{{tr('category')}}</th>
-						      <th>{{tr('action')}}</th>
-						    </tr>
-						</thead>
+									    <tr id="row{{$category->id}}">
+									      	<td>{{$i+1}}</td>
+									      	<td>{{$category->name}}</td>
 
-						<tbody>
-							@foreach($categories as $i => $category)
-
-							    <tr id="row{{$category->id}}">
-							      	<td>{{$i+1}}</td>
-							      	<td>{{$category->name}}</td>
-
-									<td>
-										<a href="edit-category/{{$category->id}}" class="btn btn-primary">Edit</a>
-										<button class="btn btn-danger" onclick="return confirmDelete({{$category->id}});">Delete</button>
-									</td>
-							    </tr>
-							@endforeach
-						</tbody>
-					</table>
-				@else
-					<h3 class="no-result">No results found</h3>
-				@endif
-            </div>
+											<td>
+												<a href="edit-category/{{$category->id}}" class="btn btn-primary">Edit</a>
+												<button class="btn btn-danger" onclick="return confirmDelete({{$category->id}});">Delete</button>
+											</td>
+									    </tr>
+									@endforeach
+								</tbody>
+							</table>
+						@else
+							<h3 class="no-result">No results found</h3>
+						@endif
+            
           </div>
         </div>
     </div>
