79fb0ef00c69a9c7c549894df8b4e5c193c396e0
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-19 20:36:43 -0400

    customizing admin

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -1,10 +1,9 @@
 <header class="main-header">
     <!-- Logo -->
     <a href="" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini">UkumbiTV - Admin</span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg">{{Setting::get('site_name')}}</span>
+    	UkumbiTV - Admin 
+      <!-- <span class="logo-mini">UkumbiTV - Admin</span> 
+        <span class="logo-lg">{{Setting::get('site_name')}}</span> -->
     </a>
 
     <!-- Header Navbar: style can be found in header.less -->
