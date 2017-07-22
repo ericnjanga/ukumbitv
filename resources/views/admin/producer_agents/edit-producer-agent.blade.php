b8b9b9fe9e1d3ff1c6315bf37ab040fefc84c876
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-22 11:57:45 -0400

    Styling admin

--- resources/views/admin/producer_agents/edit-producer-agent.blade.php
+++ resources/views/admin/producer_agents/edit-producer-agent.blade.php
@@ -57,7 +57,7 @@
               </div>
               <div class="form-group">
                 <label for="description" class="">Description </label>
-                <textarea class="form-control" rows="6" cols="50" id="description" name="description">{{$agent->description}}</textarea>
+                <textarea class="form-control" rows="7" cols="50" id="description" name="description">{{$agent->description}}</textarea>
               </div>
             </div><!-- col-md-6 -->
           </div><!-- row -->  
