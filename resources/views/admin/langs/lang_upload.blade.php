45e739b7d54428c7d5965203a20f1c2595bb16c6
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-22 11:15:56 -0400

    Styling admin

--- resources/views/admin/langs/lang_upload.blade.php
+++ resources/views/admin/langs/lang_upload.blade.php
@@ -26,48 +26,36 @@
 
 @section('content')
 
-    @include('notification.notify')
+  @include('notification.notify')
 
-
-    <div class="row">
-        <div class="col-lg-12">
-            <section>
-                <div class="wizard">
-
-
-                    <form id="lang-upload" method="POST" enctype="multipart/form-data" role="form">
-                        <div class="tab-content">
-                            <div class="tab-pane active" role="tabpanel" id="step1">
-                                <!-- <h3>Video Details</h3> -->
-                                <div style="margin-left: 15px"><small>Note : <span style="color:red">*</span> fields are mandatory. Please fill and click next.</small></div>
-                                <hr>
-                                <div class="">
-                                    <input type="hidden" value="1" name="ajax_key">
-                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                        <div class="form-group">
-                                            <label for="title" class="">Title * </label>
-                                            <input type="text" required class="form-control" id="title" name="title" placeholder="{{tr('title')}}">
-                                        </div>
-                                    </div>
-
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </form>
-
-                </div>
-            </section>
-        </div>
+<!-- 
+<div style="margin-left: 15px"><small>Note : <span style="color:red">*</span> fields are mandatory. Please fill and click next.</small></div>
+ -->
+  <div class="row">
+    <div class="col-lg-12"> 
+      <div class="box tab-content tab-content-langadd"> 
+        <form id="lang-upload" method="POST" enctype="multipart/form-data" role="form">
+          <div class="form-group col-sm-12">
+            <input type="hidden" value="1" name="ajax_key">
+            <label for="title" class="">Title * </label>
+            <input type="text" required class="form-control" id="title" name="title" placeholder="{{tr('title')}}"> 
+          </div>    
+        </form> 
+      </div> 
     </div>
-    <button class="btn btn-primary btn-info-full" id="finishBtn" onclick="createLang()">Finish</button>
+		
+
+		<div class="col-md-4 col-md-offset-8 form-group">
+	    <button class="btn btn-submit btn-block" id="finishBtn" onclick="createLang()">Finish</button>
+		</div> 
+  </div><!-- row --> 
+
+ 
 
 
-
-    <div class="overlay">
-        <div id="loading-img"></div>
-    </div>
+  <div class="overlay">
+      <div id="loading-img"></div>
+  </div>
 
 @endsection
 
