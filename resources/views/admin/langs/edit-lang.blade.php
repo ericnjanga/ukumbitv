52f38b49c0355b5c44fdc84bf9608adf08a8e1d2
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-22 11:24:21 -0400

    Styling admin

--- resources/views/admin/langs/edit-lang.blade.php
+++ resources/views/admin/langs/edit-lang.blade.php
@@ -12,45 +12,32 @@
 
     @include('notification.notify')
 
-    <div class="row">
+    <div class="row"> 
+    	<div class="col-lg-12"> 
+      	<div class="box tab-content tab-content-langedit">  
+          <form class="form-horizontal" method="POST" enctype="multipart/form-data" role="form"> 
+            <div class="row"> 
+              <input type="hidden" id="langid" name="id" value="{{$lang->id}}">
 
-        <div class="col-md-10">
+              <div class="form-group col-sm-12">
+                <label for="name">{{tr('name')}}</label>
+                <input type="text" required class="form-control" value="{{$lang->title}}" id="name" name="name">
+              </div> 
+            </div> 
+          </form> 
+        </div> 
+      </div> 
+			
 
-            <div class="box box-primary">
+			<div class="col-md-4 col-md-offset-8 form-group">
+		    <button class="btn btn-submit btn-block" id="finishBtn" onclick="editLang()">Save</button>
+			</div>
+			
 
-                <div class="box-header label-primary">
-                    <b style="font-size:18px;">Edit director</b>
-                    <a href="{{route('admin.add.lang')}}" class="btn btn-default pull-right">Add Language</a>
-                </div>
-
-                <form class="form-horizontal" method="POST" enctype="multipart/form-data" role="form">
-
-                    <div class="box-body">
-
-                        <input type="hidden" id="langid" name="id" value="{{$lang->id}}">
-
-                        <div class="form-group">
-                            <label for="name" class="col-sm-1 control-label">{{tr('name')}}</label>
-                            <div class="col-sm-10">
-                                <input type="text" required class="form-control" value="{{$lang->title}}" id="name" name="name">
-                            </div>
-                        </div>
-
-
-                    </div>
-
-
-                </form>
-
-            </div>
-
-        </div>
-
-    </div>
-    <div class="box-footer">
-        <progress id="progressbar" value="0" max="100"></progress>
-        <button class="btn btn-primary btn-info-full" id="finishBtn" onclick="editLang()">Save</button>
-    </div>
+			<div class="col-md-12 form-group">
+		    <progress id="progressbar" style="width:100%" value="0" max="100"></progress>
+			</div>
+    </div><!-- row --> 
 @endsection
 
 @section('scripts')
