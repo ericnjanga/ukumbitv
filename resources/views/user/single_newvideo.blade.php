6624b87137e6e30eadd57e504d55e850ab049ab0
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-07-30 02:13:53 -0400

    updated video single page

--- resources/views/user/single_newvideo.blade.php
+++ resources/views/user/single_newvideo.blade.php
@@ -26,7 +26,7 @@ page-singlevideo
 	<!-- ..................................  -->
 
 
-		<iframe src="https://player.vimeo.com/video/{{$videoId}}" autoplay="1" width="100%" height="auto" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		<iframe src="https://player.vimeo.com/video/{{$videoId}}&autoplay=1" autoplay="1" width="100%" height="auto" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 
 
 
