dae161133bd7aa2c1a8f0efd7915c834321bd145
zhak <zhakgo@gmail.com>
2017-08-07 19:40:26 +0300

    start

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import Hello from '@/components/Hello'
+
+Vue.use(Router)
+
+export default new Router({
+  linkActiveClass: 'active',
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: resolve => require(['@/components/main'], resolve)
+    }
+  ]
+})
