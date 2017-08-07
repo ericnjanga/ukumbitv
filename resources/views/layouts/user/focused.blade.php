cda9af1ed3a1890cee42317c6e3706f9f5938e18
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-07 19:33:30 -0400

    creating a coming soon page

--- resources/views/layouts/user/focused.blade.php
+++ resources/views/layouts/user/focused.blade.php
@@ -49,19 +49,20 @@
 </style>
 
 
-
+@include('layouts.user.focused-nav')
 		 @yield('content')
 
 
 <!--
-    @include('layouts.user.focused-nav')
+    
       --> 
     @include('layouts.user.footer') 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-    <!-- background images slider -->
+    <!-- background images slider
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>  
+     -->
     @yield('scripts')
 	</body>
 </html>
