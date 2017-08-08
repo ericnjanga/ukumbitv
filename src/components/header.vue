08145b0563ca823e8e030ff42c384a25f580ed2d
zhak <zhakgo@gmail.com>
2017-08-08 14:56:36 +0300

    main header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,56 @@
 <template>
-    <header>
-        HEADER
-    </header>
+    <div class="header-wrap">
+        <header>
+            <section class="main-header">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-sm-2">
+                            <div class="logo-block">
+                                <img src="~@/assets/img/logo.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="input-wrap search-wrap">
+                                <input type="search" name="searchvideo" placeholder="Titles, people, tags">
+                                <div class="search-list-block">
+                                    <ul class="search-list">
+                                        <li><a href="">Titles, people, tags</a></li>
+                                        <li><a href="">Titles, people, tags</a></li>
+                                        <li><a href="">Titles, people, tags</a></li>
+                                        <li><a href="">Titles, people, tags</a></li>
+                                        <li><a href="">Titles, people, tags</a></li>
+                                        <li><a href="">Titles, people, tags</a></li>
+                                    </ul>
+                                </div>
+                                <button class="butn-search">
+                                    <span class="icon icon-magnifying-glass"></span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="login-block">
+                                <div class="login-info">
+                                    <div class="login-text">100 videos left</div>
+                                    <a href="" class="login-upgrade">upgrade package</a>
+                                </div>
+                                <div class="img-block">
+                                    <span class="icon icon-man-user"></span>
+                                    <!--<img src="~@/assets/img/no-img.png" alt="">-->
+                                    <div class="account-block">
+                                        <ul class="account-list">
+                                            <li><a href="">My account</a></li>
+                                            <li><a href="">Plan</a></li>
+                                            <li><a href="">Sign Out</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </header>
+    </div>
+
 </template>
