572b841055db16db1d7eb568e9ef887ea4de2c7d
Alex T <tgnwest@gmail.com>
2017-08-08 21:14:43 +0300

    coming soon

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \App\Http\Middleware\CheckForMaintenanceMode::class,
+//        \App\Http\Middleware\CheckForMaintenanceMode::class,
         //\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
 
     ];
@@ -33,6 +33,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \App\Http\Middleware\Locale::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+           // \App\Http\Middleware\CheckForMaintenanceMode::class,
             //\App\Http\Middleware\VerifyUser::class,
             // \App\Http\Middleware\VerifyCsrfToken::class,
         ],
@@ -55,7 +56,8 @@ class Kernel extends HttpKernel
         'moderator' => \App\Http\Middleware\AuthenticateModerator::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+//        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'guest' => \App\Http\Middleware\CheckForMaintenanceMode::class,
         'guestadmin' => \App\Http\Middleware\RedirectIfAuthenticatedAdmin::class,
         'guestmoderator' => \App\Http\Middleware\RedirectIfAuthenticatedModerator::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
