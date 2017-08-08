7761b16b042c91a0c105816e0759ff2691599861
zhak <zhakgo@gmail.com>
2017-08-08 19:13:12 +0300

    main start

--- src/components/main.vue
+++ src/components/main.vue
@@ -3,7 +3,56 @@
         <div class="main-wrap">
             <div class="container">
                 <div class="row">
-                    MAIN
+                    <div class="col-sm-2">
+                        <aside>
+                            <div class="aside-block">
+                                <ul>
+                                    <li><a href="">All videos</a></li>
+                                    <li><a href="">Movies</a></li>
+                                    <li><a href="">TV Shows</a></li>
+                                    <li><a href="">Web Series</a></li>
+                                    <li><a href="">Documentaries</a></li>
+                                </ul>
+                            </div>
+                        </aside>
+                    </div>
+                    <div class="col-sm-10">
+                        <div class="main-top-block">
+                            <img src="~@/assets/img/bg-video.png" alt="">
+                            <div class="layer"></div>
+                            <div class="video-info-block">
+                                <div class="video-title">Ghost in the Shell</div>
+                                <div class="video-info-text">
+                                    <span class="age">16+</span>
+                                    <span class="date">March 2017</span>
+                                    <span class="genre">Drama, Comedy, Triller</span>
+                                </div>
+                                <div class="series-text">1 Season, 12 Series</div>
+                                <div class="actors-block">
+                                    <div class="actors-title">Actors</div>
+                                    <p class="actors-list">Scarlett Johansson, Beat Takeshi Kitano, Michael Carmen Pitt, Pilou Asbaek, Chin Han, Juliette Binoche</p>
+                                </div>
+                                <div class="butn-block">
+                                    <a href="" class="butn butn-orange butn-play upper" ><span class="icon icon-play-arrow"></span>Play</a>
+                                    <a href="" class="butn butn-orange upper">Add to list</a>
+                                    <a href="" class="butn-share">Share</a>
+                                    <a href="" class="butn-like">like</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="video-slider-block">
+                            <div class="title">New Videos</div>
+                        </div>
+                        <div class="video-slider-block">
+                            <div class="title">My List</div>
+                        </div>
+                        <div class="video-slider-block">
+                            <div class="title">Popular Videos</div>
+                        </div>
+                        <div class="video-slider-block">
+                            <div class="title">Liked Videos</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
