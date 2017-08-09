b371f350bf9c8ccd27bf1a4176bcd7a9b01ad243
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-08 21:04:56 -0400

    styling coming soon page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -18,7 +18,6 @@
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
-                border:10px solid #eee;
                 background-size: cover;
                 background-image: url("{{asset('streamtube/images/bg-youtube.jpg')}}");
             }
@@ -59,6 +58,47 @@
                     </div>
 
                 </footer>
+
+
+
+                <!-- Display the countdown timer in an element -->
+<p id="demo"></p>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 1, 2017 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
+
+
+
+
+
             </div>
         </div>
     </body>
