8d45026864659b0f738ffcb7408f02d9e5bf882f
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-12 04:11:03 -0400

    updating coming soon page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -14,19 +14,19 @@
   <script type="text/javascript">
   !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
   </script>
-  <link href="images/598c864bc327c10001ec1587_Favicon2032.png" rel="shortcut icon" type="image/x-icon">
+  <link href="{{asset('streamtube/images/598c864bc327c10001ec1587_Favicon2032.png')}}" rel="shortcut icon" type="image/x-icon">
   <link href="images/598c8655153f32000197a4b0_Favicon20256.png" rel="apple-touch-icon">
- <link rel="stylesheet" type="text/css" href="css/style.css">
+	    <link rel="stylesheet" type="text/css" href="{{asset('streamtube/css/utv-comingsoon.css')}}">  
 </head>
 
 <body>
   <div class="section">
     <div class="w-row">
       <div class="w-col w-col-3">
-        <img class="image" src="images/598c803a153f320001979d21_UkumbiTV-logo.png" width="250">
+        <img class="image" src="{{asset('streamtube/images/598c803a153f320001979d21_UkumbiTV-logo.png')}}" width="250">
       </div>
       <div class="w-col w-col-6">
-        <img src="images/598c807fc327c10001ec106f_D0A1D0BBD0BED0B9-1.png">
+        <img src="{{asset('streamtube/images/598c807fc327c10001ec106f_D0A1D0BBD0BED0B9-1.png')}}">
       </div>
       <div class="w-col w-col-3">
       </div>
