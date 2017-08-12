792357b18b788a1a385c8a921cb43fcba845ce63
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-12 04:19:35 -0400

    updating coming soon page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -36,9 +36,55 @@
       </div>
       <div class="h1">Enjoy
         <span class="text-span">Africa's</span>finest cinema</div>
-      <script id="nstimerscr" class="timer" type="text/javascript" src="https://timer.aglichev.ru/timer/e10dbb0cec812df6f6f4a4de73a90925.js"></script>
+
+
+
+        <!-- Display the countdown timer in an element -->
+				<p id="countdown"></p>
+      <!-- <script id="nstimerscr" class="timer" type="text/javascript" src="https://timer.aglichev.ru/timer/e10dbb0cec812df6f6f4a4de73a90925.js"></script> -->
     </div>
   </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 1, 2017 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="countdown"
+  document.getElementById("countdown").innerHTML = days + "<span>:</span>" + hours + "<span>:</span>" + minutes + "<span>:</span>" + seconds;
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
   
 </body>
 
