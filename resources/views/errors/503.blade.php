62d3672d783736303bb05f20e8ba4ffb0d3b5ecf
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-12 04:08:54 -0400

    updating coming soon page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,118 +1,45 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-              height: 100%;
-            }
-
-            body {
-              margin: 0;
-              padding: 0;
-              width: 100%;
-              color: #333;
-              display: table;
-              /*font-weight: 100;*/
-              font-family: 'Lato';
-              background-size: cover;
-              background-image: url("{{asset('streamtube/images/bg-youtube.jpg')}}");
-            }
-
-            .container {
-              text-align: center;
-              display: table-cell;
-              vertical-align: top;
-            }
-
-            .content {
-              text-align: center;
-              display: inline-block;
-            }
-
-            .title {
-            	margin-top: 20px;
-              font-size: 30px;
-              text-transform: uppercase; 
-            }
-            #demo {
-            	margin: 20px;
-            	font-size: 70px;
-            } 
-            .btn-link {
-            	padding: 10px;
-					    background: rgba(0,0,0,0.2);
-					    border: 0;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Lauching soon</div>
-
-                <!-- Display the countdown timer in an element -->
-								<p id="demo"></p>
 
 
+<html data-wf-domain="ukumbitv.webflow.io" data-wf-page="5678f447c23895ea308e187e" data-wf-site="5678f447c23895ea308e187d" data-wf-status="1">
 
+<head>
+  <meta charset="utf-8">
+  <title>UkumbiTV - Lauching soon</title>
+  <meta content="Watch unlimited African movies and TV Shows online" name="description">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  
+  
+  <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif]-->
+  <script type="text/javascript">
+  !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
+  </script>
+  <link href="images/598c864bc327c10001ec1587_Favicon2032.png" rel="shortcut icon" type="image/x-icon">
+  <link href="images/598c8655153f32000197a4b0_Favicon20256.png" rel="apple-touch-icon">
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
 
-                <footer class="text-center">
-                    <div class="fb-login">
-                        @if(config('services.facebook.client_id') && config('services.facebook.client_secret'))
-                            <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
-                                <input type="hidden" value="facebook" name="provider" id="provider">
-                                <button type="submit" class="btn-link">
-                                    <i class="fa fa-facebook-official" aria-hidden="true"></i>
-                                    Admin Login
-                                </button>
-                            </form>
-                        @endif
-                    </div>
+<body>
+  <div class="section">
+    <div class="w-row">
+      <div class="w-col w-col-3">
+        <img class="image" src="images/598c803a153f320001979d21_UkumbiTV-logo.png" width="250">
+      </div>
+      <div class="w-col w-col-6">
+        <img src="images/598c807fc327c10001ec106f_D0A1D0BBD0BED0B9-1.png">
+      </div>
+      <div class="w-col w-col-3">
+      </div>
+    </div>
+    <div class="w-container">
+      <div class="line">
+      </div>
+      <div class="h1">Enjoy
+        <span class="text-span">Africa's</span>finest cinema</div>
+      <script id="nstimerscr" class="timer" type="text/javascript" src="//timer.aglichev.ru/timer/e10dbb0cec812df6f6f4a4de73a90925.js"></script>
+    </div>
+  </div>
+  
+</body>
 
-                </footer>
-
-
-
-
-<script>
-// Set the date we're counting down to
-var countDownDate = new Date("Sep 1, 2017 00:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "<span>:</span>" + hours + "<span>:</span>" + minutes + "<span>:</span>" + seconds;
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-</script>
-
-
-
-
-
-            </div>
-        </div>
-    </body>
 </html>
