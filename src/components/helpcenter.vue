d34334bdb1d83d1a9fca5300620c1e41911a3836
zhak <zhakgo@gmail.com>
2017-08-15 11:08:01 +0300

    helpcenter

--- src/components/helpcenter.vue
+++ src/components/helpcenter.vue
@@ -2,8 +2,35 @@
     <main>
         <div class="helpcenter-wrap">
             <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-5">
+                        <div class="title-page">Help center</div>
+                    </div>
+                </div>
                 <div class="row">
-                    helpcenter
+                    <div class="col-sm-2">
+                        <ul class="chapter-list">
+                            <li><a href=""><span class="square"></span>My account</a></li>
+                            <li><a href=""><span class="square"></span>Payments</a></li>
+                            <li><a href=""><span class="square"></span>Packages</a></li>
+                            <li><a href=""><span class="square"></span>Packages</a></li>
+                            <li><a href=""><span class="square"></span>Other</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 offset-sm-1">
+                        <ul class="article-list">
+                            <li><a href="">TV Shows & Movies</a></li>
+                            <li><a href="">Your plan will determine</a></li>
+                            <li><a href="">If you choose to remain a member of</a></li>
+                            <li><a href="">Your plan will determine</a></li>
+                            <li><a href="">If you choose to remain a member of</a></li>
+                        </ul>
+                        <div class="content-block-bottom">
+                            <div class="title-page-add">No Answer ?</div>
+                            <div class="text-add">Fill free to contact us</div>
+                            <a href="" class="butn butn-orange butn-large">Contact us</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
