7ad9aa81986f2e443e165e58c3a7f16841b41edf
Alex T <tgnwest@gmail.com>
2017-08-17 20:40:38 +0300

    test dev branch

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-//        \App\Http\Middleware\CheckForMaintenanceMode::class,
+    //    \App\Http\Middleware\CheckForMaintenanceMode::class,
         //\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
 
     ];
