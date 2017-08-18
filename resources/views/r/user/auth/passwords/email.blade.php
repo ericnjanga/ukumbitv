688bfe109a9081f05c53eb45150cf8204371499a
zhak <zhakgo@gmail.com>
2017-08-18 10:35:28 +0300

    sign in,sign up, about, privacy, reset pass mobile

--- resources/views/r/user/auth/passwords/email.blade.php
+++ resources/views/r/user/auth/passwords/email.blade.php
@@ -3,12 +3,12 @@
     <div class="resetpas-wrap">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-sm-3">
+                <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3">
                     <div class="title-page">Reset password</div>
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col-sm-4">
+                <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="notice-block">
                         <div class="text-add">We will send new password on your e-mail</div>
                         <form  action="" method="">
