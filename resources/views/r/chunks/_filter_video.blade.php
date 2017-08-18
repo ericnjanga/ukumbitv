8ba3254e7579d6121965894d3579566b2c50efec
Andriy Lozynskiy <alozynskiy@gmail.com>
2017-08-18 14:17:27 +0300

    disable new menu

--- resources/views/r/chunks/_filter_video.blade.php
+++ resources/views/r/chunks/_filter_video.blade.php
@@ -1,61 +1,61 @@
 <aside>
-    <div class="menu-container">
-        <div class="show-entries-hover hidden">
-            <span class="hover-arrow"></span>
-        </div>
+    {{--<div class="menu-container">--}}
+        {{--<div class="show-entries-hover hidden">--}}
+            {{--<span class="hover-arrow"></span>--}}
+        {{--</div>--}}
 
-            <ul class="play-menu">
-                <li id="store">
-                    <a href="#">
-                        <span class="menu-entry-icon store"></span>
-                        <span class="menu-entry-text">All videos</span>
-                    </a>
-                </li>
-                <li id="apps">
-                    <a href="#">
-                        <span class="menu-entry-icon apps"></span>
-                        <span class="menu-entry-text">Movies</span>
-                    </a>
-                    <ul class="sub-menu">
-                        <li class="main-link">My Apps</li>
-                        <li class="main-link highlight">Shop</li>
-                        <li class="sub-menu-divider"></li>
-                        <li class="secondary-link">Games</li>
-                        <li class="secondary-link">Editors' Choice</li>
-                    </ul>
-                </li>
-                <li id="movies">
-                    <a href="#">
-                        <span class="menu-entry-icon movies"></span>
-                        <span class="menu-entry-text">TV Shows</span>
-                    </a>
-                    <ul class="sub-menu">
-                        <li class="main-link">My movies</li>
-                        <li class="main-link highlight">Shop</li>
-                        <li class="sub-menu-divider"></li>
-                        <li class="secondary-link">Studios</li>
-                    </ul>
-                </li>
-                <li id="music">
-                    <a href="#">
-                        <span class="menu-entry-icon music"></span>
-                        <span class="menu-entry-text">Web Series</span>
-                    </a>
-                    <ul class="sub-menu">
-                        <li class="main-link">My music</li>
-                        <li class="main-link highlight">Shop</li>
-                    </ul>
-                </li>
-                <li id="books">
-                    <a href="#">
-                        <span class="menu-entry-icon books"></span>
-                        <span class="menu-entry-text">Documentaries</span>
-                    </a>
-                    <ul class="sub-menu">
-                        <li class="main-link">My books</li>
-                        <li class="main-link highlight">Shop</li>
-                    </ul>
-                </li>
+            {{--<ul class="play-menu">--}}
+                {{--<li id="store">--}}
+                    {{--<a href="#">--}}
+                        {{--<span class="menu-entry-icon store"></span>--}}
+                        {{--<span class="menu-entry-text">All videos</span>--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li id="apps">--}}
+                    {{--<a href="#">--}}
+                        {{--<span class="menu-entry-icon apps"></span>--}}
+                        {{--<span class="menu-entry-text">Movies</span>--}}
+                    {{--</a>--}}
+                    {{--<ul class="sub-menu">--}}
+                        {{--<li class="main-link">My Apps</li>--}}
+                        {{--<li class="main-link highlight">Shop</li>--}}
+                        {{--<li class="sub-menu-divider"></li>--}}
+                        {{--<li class="secondary-link">Games</li>--}}
+                        {{--<li class="secondary-link">Editors' Choice</li>--}}
+                    {{--</ul>--}}
+                {{--</li>--}}
+                {{--<li id="movies">--}}
+                    {{--<a href="#">--}}
+                        {{--<span class="menu-entry-icon movies"></span>--}}
+                        {{--<span class="menu-entry-text">TV Shows</span>--}}
+                    {{--</a>--}}
+                    {{--<ul class="sub-menu">--}}
+                        {{--<li class="main-link">My movies</li>--}}
+                        {{--<li class="main-link highlight">Shop</li>--}}
+                        {{--<li class="sub-menu-divider"></li>--}}
+                        {{--<li class="secondary-link">Studios</li>--}}
+                    {{--</ul>--}}
+                {{--</li>--}}
+                {{--<li id="music">--}}
+                    {{--<a href="#">--}}
+                        {{--<span class="menu-entry-icon music"></span>--}}
+                        {{--<span class="menu-entry-text">Web Series</span>--}}
+                    {{--</a>--}}
+                    {{--<ul class="sub-menu">--}}
+                        {{--<li class="main-link">My music</li>--}}
+                        {{--<li class="main-link highlight">Shop</li>--}}
+                    {{--</ul>--}}
+                {{--</li>--}}
+                {{--<li id="books">--}}
+                    {{--<a href="#">--}}
+                        {{--<span class="menu-entry-icon books"></span>--}}
+                        {{--<span class="menu-entry-text">Documentaries</span>--}}
+                    {{--</a>--}}
+                    {{--<ul class="sub-menu">--}}
+                        {{--<li class="main-link">My books</li>--}}
+                        {{--<li class="main-link highlight">Shop</li>--}}
+                    {{--</ul>--}}
+                {{--</li>--}}
                 {{--<li id="devices">--}}
                     {{--<a href="#">--}}
                         {{--<span class="menu-entry-icon devices"></span>--}}
@@ -65,69 +65,69 @@
                         {{--<li class="main-link highlight">Shop</li>--}}
                     {{--</ul>--}}
                 {{--</li>--}}
-            </ul>
+            {{--</ul>--}}
 
-    </div>
-    {{--<div class="aside-block">--}}
-        {{--<ul class="aside-menu">--}}
-            {{--<li>--}}
-                {{--<a href="" class="all">--}}
-                    {{--<span class="icon icon-folder"></span>--}}
-                    {{--<span class="list-text">All videos</span>--}}
-                {{--</a>--}}
-            {{--</li>--}}
-            {{--<li>--}}
-                {{--<a href="" class="movies">--}}
-                    {{--<span class="icon icon-film"></span>--}}
-                    {{--<span class="list-text">Movies</span>--}}
-                {{--</a>--}}
-                {{--<div class="sub-menu-block">--}}
-                    {{--<div class="sub-menu-butn"></div>--}}
-                    {{--<ul class="sub-menu">--}}
-                        {{--<li>--}}
-                            {{--<a href="{{route('user.category',0)}}">--}}
-                                {{--All movies--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                        {{--<li>--}}
-                            {{--<a href="{{route('user.category',0)}}">--}}
-                                {{--<span class="list-text">Short</span>--}}
-                                {{--<span class="time">10 min</span>--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                        {{--<li>--}}
-                            {{--<a href="{{route('user.category',0)}}">--}}
-                                {{--<span class="list-text">Medium</span>--}}
-                                {{--<span class="time">45 min max</span>--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                        {{--<li>--}}
-                            {{--<a href="{{route('user.category',0)}}">--}}
-                                {{--<span class="list-text">Feature</span>--}}
-                                {{--<span class="time">45 min up</span>--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                    {{--</ul>--}}
-                {{--</div>--}}
-            {{--</li>--}}
-            {{--<li>--}}
-                {{--<a href="" class="shows">--}}
-                    {{--<span class="icon icon-television"></span>--}}
-                    {{--<span class="list-text">TV Shows</span>--}}
-                {{--</a>--}}
-            {{--</li>--}}
-            {{--<li>--}}
-                {{--<a href="" class="web">--}}
-                    {{--<span class="icon icon-screen_play"></span>--}}
-                    {{--<span class="list-text">Web Series</span>--}}
-                {{--</a>--}}
-            {{--</li>--}}
-            {{--<li>--}}
-                {{--<a href="" class="doc">--}}
-                    {{--<span class="icon icon-film-roll"></span>--}}
-                    {{--<span class="list-text">Documentaries</span>--}}
-                {{--</a>--}}
-            {{--</li>--}}
-        {{--</ul>--}}
     {{--</div>--}}
+    <div class="aside-block">
+        <ul class="aside-menu">
+            <li>
+                <a href="" class="all">
+                    <span class="icon icon-folder"></span>
+                    <span class="list-text">All videos</span>
+                </a>
+            </li>
+            <li>
+                <a href="" class="movies">
+                    <span class="icon icon-film"></span>
+                    <span class="list-text">Movies</span>
+                </a>
+                <div class="sub-menu-block">
+                    <div class="sub-menu-butn"></div>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="{{route('user.category',0)}}">
+                                All movies
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{route('user.category',0)}}">
+                                <span class="list-text">Short</span>
+                                <span class="time">10 min</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{route('user.category',0)}}">
+                                <span class="list-text">Medium</span>
+                                <span class="time">45 min max</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{route('user.category',0)}}">
+                                <span class="list-text">Feature</span>
+                                <span class="time">45 min up</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <a href="" class="shows">
+                    <span class="icon icon-television"></span>
+                    <span class="list-text">TV Shows</span>
+                </a>
+            </li>
+            <li>
+                <a href="" class="web">
+                    <span class="icon icon-screen_play"></span>
+                    <span class="list-text">Web Series</span>
+                </a>
+            </li>
+            <li>
+                <a href="" class="doc">
+                    <span class="icon icon-film-roll"></span>
+                    <span class="list-text">Documentaries</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </aside>
