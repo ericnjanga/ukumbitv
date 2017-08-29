4a6b808d636bbcad1715621406a40ffea5c6275e
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-28 23:37:05 -0400

    improving layout

--- resources/views/r/chunks/_header_search.blade.php
+++ resources/views/r/chunks/_header_search.blade.php
@@ -7,23 +7,22 @@
               <img src="{{asset('r/img/logo.png')}}" alt="">
           </a>
       </div>
-      <div class="col-sm-6 col-md-6 col-lg-7 col-xl-6 search-block">
-          <form action="{{route('search-all')}}" method="post">
-              <div class="input-wrap search-wrap">
-                  <input type="search" name="key" id="search-input" placeholder="Titles, people, tags" onclick="getSearchData()">
-                  <div class="search-list-block">
-                      <ul class="search-list">
-                      </ul>
-                  </div>
-                  <button id="butn-search" type="submit" class="butn-search">
-                      <span class="icon icon-magnifying-glass"></span>
-                  </button>
-              </div>
-          </form>
-
+      <div class="frame-search col-sm-6 col-md-6 col-lg-7 col-xl-6 search-block">
+        <form action="{{route('search-all')}}" method="post">
+            <div class="input-wrap search-wrap">
+                <input type="search" name="key" id="search-input" placeholder="Titles, people, tags" onclick="getSearchData()">
+                <div class="search-list-block">
+                    <ul class="search-list">
+                    </ul>
+                </div>
+                <button id="butn-search" type="submit" class="butn-search">
+                    <span class="icon icon-magnifying-glass"></span>
+                </button>
+            </div>
+        </form> 
       </div>
       <div class="col-sm-6 col-md-3 col-lg-3 col-xl-4">
-        <a style="position:absolute;" href="{{route('user.reset-trial')}}">RESET TRIAL</a>
+        <!-- <a style="position:absolute;" href="{{route('user.reset-trial')}}">RESET TRIAL</a> -->
           @include('r.chunks._login_block')
       </div>
     </div>
