855d1e2d7c1f1b6dc9a9f20f4b8760d1bd31962a
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-29 00:15:29 -0400

    improving layout

--- resources/views/r/chunks/_header_search.blade.php
+++ resources/views/r/chunks/_header_search.blade.php
@@ -7,7 +7,7 @@
             <img src="{{asset('r/img/logo.png')}}" alt="">
         </a>
       </div>
-      <div class="frame-search col-sm-6 col-md-6 col-lg-7 col-xl-6 search-block">
+      <div id="frame-search" class="frame-search col-sm-6 col-md-6 col-lg-7 col-xl-6 search-block">
         <form action="{{route('search-all')}}" method="post">
           <!-- <div class="input-wrap search-wrap">
               <input type="search" name="key" id="search-input" class="search-input" placeholder="{{trans('messages.Search_placeholder')}}" autocomplete="false" onclick="getSearchData()">
@@ -19,8 +19,9 @@
                   <span class="icon icon-magnifying-glass"></span>
               </button>
             </div> -->
+
           <div class="input-group">
-			      <input type="text" class="form-control" placeholder="Search for...">
+			      <input type="text" id="search-input" class="form-control search-input typeahead" placeholder="{{trans('messages.Search_placeholder')}}" autocomplete="false" onclick="getSearchData()">
 			      <span class="input-group-btn">
 			        <button class="btn btn-default" type="button">Go!</button>
 			      </span>
--- resources/views/r/layouts/app.blade.php
+++ resources/views/r/layouts/app.blade.php
@@ -38,10 +38,69 @@
     <!-- lightbox librairy
     <script type="text/javascript" src="{{asset('r/js/jquery.colorbox-min.js')}}"></script>
   -->
+
+		<!-- auto complete librairy -->
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.jquery.min.js"></script>
+
     <script type="text/javascript" src="{{asset('r/js/script.js')}}"></script>
     <script type="text/javascript" src="{{asset('r/js/Google-play-menu.js')}}"></script>
     <script type="text/javascript" src="{{asset('packages/sweetalert/sweetalert.min.js')}}"></script>
     <script>
+
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+$('#frame-search .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'states',
+  source: substringMatcher(states)
+});
+
+
+
+
+
+
+
+
+
+
+    
         var searchData;
         var searchList = '';
 
