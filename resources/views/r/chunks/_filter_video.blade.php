3645157572a2c4848f837b40061821f569bc51bd
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-29 02:32:03 -0400

    improving layout

--- resources/views/r/chunks/_filter_video.blade.php
+++ resources/views/r/chunks/_filter_video.blade.php
@@ -1,5 +1,5 @@
 <div id="GP-menu" class="GP-menu">
-	<div class="show-entries-hover hidden">
+	<!-- <div class="show-entries-hover hidden">
 		<span class="hover-arrow"></span>
 	</div>
 	<ul class="play-menu">
@@ -63,7 +63,95 @@
 				<li class="main-link highlight">Shop</li>
 			</ul>
 		</li>
-	</ul>
+	</ul> -->
+
+
+
+		
+
+
+
+
+	<!-- <aside>
+    <div class="aside-block">
+        <div class="menu-container">
+
+            <ul class="aside-menu menu-items">
+                <li class="active">
+                    <a href="{{url('category/0')}}" class="all">
+                        <span class="icon icon-folder"></span>
+                        <span class="list-text">All videos</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{url('category/1')}}" class="movies">
+                        <span class="icon icon-film"></span>
+                        <span class="list-text">Movies</span>
+                    </a>
+                    <div class="sub-menu-block">
+                        <div class="show-on-hover sub-menu-butn"></div>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="{{url('category/0')}}">
+                                    <span class="list-text">
+                                        All movies
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{url('category/0')}}">
+                                    <span class="list-text">Short</span>
+                                    <span class="time">10 min</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{url('category/0')}}">
+                                    <span class="list-text">Medium</span>
+                                    <span class="time">45 min max</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{url('category/0')}}">
+                                    <span class="list-text">Feature</span>
+                                    <span class="time">45 min up</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="{{url('category/4')}}" class="shows">
+                        <span class="icon icon-television"></span>
+                        <span class="list-text">TV Shows</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{url('category/3')}}" class="web">
+                        <span class="icon icon-screen_play"></span>
+                        <span class="list-text">Web Series</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{url('category/2')}}" class="doc">
+                        <span class="icon icon-film-roll"></span>
+                        <span class="list-text">Documentaries</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</aside> -->
+</div>
+
+
+
+
+
+
+
+
+
 
 
 
@@ -146,81 +234,5 @@
 
 
 	</div>
-		
-
-
-
-
-	<!-- <aside>
-    <div class="aside-block">
-        <div class="menu-container">
-
-            <ul class="aside-menu menu-items">
-                <li class="active">
-                    <a href="{{url('category/0')}}" class="all">
-                        <span class="icon icon-folder"></span>
-                        <span class="list-text">All videos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{url('category/1')}}" class="movies">
-                        <span class="icon icon-film"></span>
-                        <span class="list-text">Movies</span>
-                    </a>
-                    <div class="sub-menu-block">
-                        <div class="show-on-hover sub-menu-butn"></div>
-                        <ul class="sub-menu">
-                            <li>
-                                <a href="{{url('category/0')}}">
-                                    <span class="list-text">
-                                        All movies
-                                    </span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{url('category/0')}}">
-                                    <span class="list-text">Short</span>
-                                    <span class="time">10 min</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{url('category/0')}}">
-                                    <span class="list-text">Medium</span>
-                                    <span class="time">45 min max</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{url('category/0')}}">
-                                    <span class="list-text">Feature</span>
-                                    <span class="time">45 min up</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="{{url('category/4')}}" class="shows">
-                        <span class="icon icon-television"></span>
-                        <span class="list-text">TV Shows</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{url('category/3')}}" class="web">
-                        <span class="icon icon-screen_play"></span>
-                        <span class="list-text">Web Series</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{url('category/2')}}" class="doc">
-                        <span class="icon icon-film-roll"></span>
-                        <span class="list-text">Documentaries</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-
-    </div>
-</aside> -->
-</div>
 
 
