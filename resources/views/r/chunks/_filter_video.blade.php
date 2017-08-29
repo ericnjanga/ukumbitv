7f082979f6d19b67d2a80c1af4b43f1e1ae955c0
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-29 02:30:36 -0400

    improving layout

--- resources/views/r/chunks/_filter_video.blade.php
+++ resources/views/r/chunks/_filter_video.blade.php
@@ -65,6 +65,92 @@
 		</li>
 	</ul>
 
+
+
+	<div class="nav-test">
+		
+
+
+
+
+		<div class="menu-container">
+			<div class="show-entries-hover hidden">
+				<span class="hover-arrow"></span>
+			</div>
+			<ul class="play-menu">
+				<li id="store">
+					<a href="#">
+						<span class="menu-entry-icon store"></span>
+						<span class="menu-entry-text">Store</span>
+					</a>
+				</li>
+				<li id="apps">
+					<a href="#">
+						<span class="menu-entry-icon apps"></span>
+						<span class="menu-entry-text">Apps</span>
+					</a>
+					<ul class="sub-menu">
+						<li class="main-link">My Apps</li>
+						<li class="main-link highlight">Shop</li>
+						<li class="sub-menu-divider"></li>
+						<li class="secondary-link">Games</li>
+						<li class="secondary-link">Editors' Choice</li>
+					</ul>
+				</li>
+				<li id="movies">
+					<a href="#">
+						<span class="menu-entry-icon movies"></span>
+						<span class="menu-entry-text">Movies</span>
+					</a>
+					<ul class="sub-menu">
+						<li class="main-link">My movies</li>
+						<li class="main-link highlight">Shop</li>
+						<li class="sub-menu-divider"></li>
+						<li class="secondary-link">Studios</li>
+					</ul>
+				</li>
+				<li id="music">
+					<a href="#">
+						<span class="menu-entry-icon music"></span>
+						<span class="menu-entry-text">Music</span>
+					</a>
+					<ul class="sub-menu">
+						<li class="main-link">My music</li>
+						<li class="main-link highlight">Shop</li>
+					</ul>
+				</li>
+				<li id="books">
+					<a href="#">
+						<span class="menu-entry-icon books"></span>
+						<span class="menu-entry-text">Books</span>
+					</a>
+					<ul class="sub-menu">
+						<li class="main-link">My books</li>
+						<li class="main-link highlight">Shop</li>
+					</ul>
+				</li>
+				<li id="devices">
+					<a href="#">
+						<span class="menu-entry-icon devices"></span>
+						<span class="menu-entry-text">Devices</span>
+					</a>
+					<ul class="sub-menu">
+						<li class="main-link highlight">Shop</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+
+
+
+
+	</div>
+		
+
+
+
+
 	<!-- <aside>
     <div class="aside-block">
         <div class="menu-container">
