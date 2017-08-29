4a3bdf9e32c84da2a7bbcaf1534e00dd38534d7f
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-28 23:59:45 -0400

    improving layout

--- resources/views/r/chunks/_header_search.blade.php
+++ resources/views/r/chunks/_header_search.blade.php
@@ -3,22 +3,28 @@
   <div class="container-fluid">
     <div class="row">
       <div class="frame-logo col-sm-4 col-md-3 col-lg-2 col-xl-2">
-          <a href="/" class="logo-block">
-              <img src="{{asset('r/img/logo.png')}}" alt="">
-          </a>
+        <a href="/" class="logo-block">
+            <img src="{{asset('r/img/logo.png')}}" alt="">
+        </a>
       </div>
       <div class="frame-search col-sm-6 col-md-6 col-lg-7 col-xl-6 search-block">
         <form action="{{route('search-all')}}" method="post">
-            <div class="input-wrap search-wrap">
-                <input type="search" name="key" id="search-input" class="search-input" placeholder="{{trans('messages.Search_placeholder')}}" autocomplete="false" onclick="getSearchData()">
-                <div class="search-list-block">
-                  <ul class="search-list">
-                  </ul>
-                </div>
-                <button id="butn-search" type="submit" class="butn-search">
-                    <span class="icon icon-magnifying-glass"></span>
-                </button>
-            </div>
+          <!-- <div class="input-wrap search-wrap">
+              <input type="search" name="key" id="search-input" class="search-input" placeholder="{{trans('messages.Search_placeholder')}}" autocomplete="false" onclick="getSearchData()">
+              <div class="search-list-block">
+                <ul class="search-list">
+                </ul>
+              </div>
+              <button id="butn-search" type="submit" class="butn-search">
+                  <span class="icon icon-magnifying-glass"></span>
+              </button>
+            </div> -->
+          <div class="input-group">
+			      <input type="text" class="form-control" placeholder="Search for...">
+			      <span class="input-group-btn">
+			        <button class="btn btn-default" type="button">Go!</button>
+			      </span>
+			    </div><!-- /input-group -->
         </form> 
       </div>
       <div class="frame-useraccount col-sm-6 col-md-3 col-lg-3 col-xl-4">
