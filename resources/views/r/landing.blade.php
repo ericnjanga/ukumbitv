28311413330aa7cf4787febd2c088786272a4724
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-29 23:59:35 -0400

    improving layout

--- resources/views/r/landing.blade.php
+++ resources/views/r/landing.blade.php
@@ -30,74 +30,68 @@
             </div>
         </div> 
 
-        <div class="price-wrap">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-sm-8">
-                        <div class="title-text">{{trans('messages.home_plansec_title')}}</div>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-10">
-                        <div class="price-list-block">
-                            <div class="price-block">
-                                @foreach($payment_plans as $payment_plan)
-                                    <div class="price-item">
-                                        <div class="price-title">{{$payment_plan->name}}</div>
-                                        <div class="count-text">{{$payment_plan->description}}</div>
-                                        <ul class="includ-list">
-                                            @php($i=1)
-                                            @while(isset($payment_plan->{'product'.$i}))
-                                                <li>{!! $payment_plan->{'product'.$i} !!}</li>
-                                                @php($i++)
-                                            @endwhile
-                                        </ul>
-                                        <div class="price upper">{{$payment_plan->price == '0'?'Free':'$ '.$payment_plan->price}}</div>
-                                    </div>
-                                @endforeach
+    
+        <div class="container"> 
+          <div class="row">
+            <div class="col-sm-12"> 
+            	<h2 class="text-center">{{trans('messages.home_plansec_title')}}</h2>
+              <div class="price-block">
+                  @foreach($payment_plans as $payment_plan)
+                      <div class="price-item">
+                          <div class="price-title">{{$payment_plan->name}}</div>
+                          <div class="count-text">{{$payment_plan->description}}</div>
+                          <ul class="includ-list">
+                              @php($i=1)
+                              @while(isset($payment_plan->{'product'.$i}))
+                                  <li>{!! $payment_plan->{'product'.$i} !!}</li>
+                                  @php($i++)
+                              @endwhile
+                          </ul>
+                          <div class="price upper">{{$payment_plan->price == '0'?'Free':'$ '.$payment_plan->price}}</div>
+                      </div>
+                  @endforeach
 
 
-                                {{--<div class="price-item">--}}
-                                {{--<div class="price-title">Silver</div>--}}
-                                {{--<div class="count-text">30 videos</div>--}}
-                                {{--<ul class="includ-list">--}}
-                                {{--<li>advantage 1</li>--}}
-                                {{--<li>another</li>--}}
-                                {{--<li>advantage 1</li>--}}
-                                {{--</ul>--}}
-                                {{--<div class="price"><span>$</span> 3</div>--}}
-                                {{--</div>--}}
-                                {{--<div class="price-item">--}}
-                                {{--<div class="price-title">Gold</div>--}}
-                                {{--<div class="count-text">100 videos</div>--}}
-                                {{--<ul class="includ-list">--}}
-                                {{--<li>advantage 1</li>--}}
-                                {{--<li>another</li>--}}
-                                {{--<li>advantage 1</li>--}}
-                                {{--<li>another</li>--}}
-                                {{--<li>advantage 1</li>--}}
-                                {{--</ul>--}}
-                                {{--<div class="price"><span>$</span> 5</div>--}}
-                                {{--</div>--}}
-                                {{--<div class="price-item">--}}
-                                {{--<div class="price-title">Platinum</div>--}}
-                                {{--<div class="count-text">150 videos</div>--}}
-                                {{--<ul class="includ-list">--}}
-                                {{--<li>advantage 1</li>--}}
-                                {{--<li>another</li>--}}
-                                {{--<li>advantage 1</li>--}}
-                                {{--<li>another</li>--}}
-                                {{--<li>advantage 1</li>--}}
-                                {{--</ul>--}}
-                                {{--<div class="price"><span>$</span> 7</div>--}}
-                                {{--</div>--}}
-                            </div>
-                            <a href="{{route('user.register.form')}}" class="butn butn-orange-white butn-large">{{trans('messages.home_cta')}}</a>
-                        </div>
-                    </div>
-                </div>
+                  {{--<div class="price-item">--}}
+                  {{--<div class="price-title">Silver</div>--}}
+                  {{--<div class="count-text">30 videos</div>--}}
+                  {{--<ul class="includ-list">--}}
+                  {{--<li>advantage 1</li>--}}
+                  {{--<li>another</li>--}}
+                  {{--<li>advantage 1</li>--}}
+                  {{--</ul>--}}
+                  {{--<div class="price"><span>$</span> 3</div>--}}
+                  {{--</div>--}}
+                  {{--<div class="price-item">--}}
+                  {{--<div class="price-title">Gold</div>--}}
+                  {{--<div class="count-text">100 videos</div>--}}
+                  {{--<ul class="includ-list">--}}
+                  {{--<li>advantage 1</li>--}}
+                  {{--<li>another</li>--}}
+                  {{--<li>advantage 1</li>--}}
+                  {{--<li>another</li>--}}
+                  {{--<li>advantage 1</li>--}}
+                  {{--</ul>--}}
+                  {{--<div class="price"><span>$</span> 5</div>--}}
+                  {{--</div>--}}
+                  {{--<div class="price-item">--}}
+                  {{--<div class="price-title">Platinum</div>--}}
+                  {{--<div class="count-text">150 videos</div>--}}
+                  {{--<ul class="includ-list">--}}
+                  {{--<li>advantage 1</li>--}}
+                  {{--<li>another</li>--}}
+                  {{--<li>advantage 1</li>--}}
+                  {{--<li>another</li>--}}
+                  {{--<li>advantage 1</li>--}}
+                  {{--</ul>--}}
+                  {{--<div class="price"><span>$</span> 7</div>--}}
+                  {{--</div>--}}
+              </div>
+              <a href="{{route('user.register.form')}}" class="butn butn-orange-white butn-large">{{trans('messages.home_cta')}}</a> 
             </div>
+          </div>
         </div>
+        
 
         <div class="bottom-text-wrap">
           <div class="container"> 
