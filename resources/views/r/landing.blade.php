9c35977d2e9bd5a786553762bb826c6bf9150f92
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-08-30 00:29:06 -0400

    improving layout

--- resources/views/r/landing.blade.php
+++ resources/views/r/landing.blade.php
@@ -102,7 +102,7 @@
       </div>
       
 
-      <div class="bottom-text-wrap">
+      <div class="">
         <div class="container"> 
           <div class="row">
             <div class="col-md-6 col-md-offset-3">
