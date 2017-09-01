8ac44a4187c51f3eb9b082fe97c745f2b9442001
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-01 12:58:37 -0400

    Improving layout

--- resources/views/r/user/auth/login.blade.php
+++ resources/views/r/user/auth/login.blade.php
@@ -1,41 +1,34 @@
 @extends('r.layouts.simple')
 @section('content')
 
-
-<div class="signin-wrap">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4">
-                <div class="sign-block">
-                    <div class="title-page">Sign In</div>
-                    <div class="text-add">The easiest way for you to sign in is with Facebook</div>
-                    <a href="" class="butn btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>Sign in with Facebook</a>
-                    <div class="or-line upper">or</div>
-                    <form action="{{route('user.login.post')}}" method="POST">
-                        <div class="input-wrap">
-                            <label>Enter your e-mail <span>*</span></label>
-                            <input type="email" name="email" required>
-                        </div>
-                        <div class="input-wrap">
-                            <label>Password <span>*</span></label>
-                            <input type="password" name="password" required>
-                        </div>
-                        <div class="operations-pass">
-                            <div class="input-wrap checkbox-wrap">
-                                <input id="remember" type="checkbox">
-                                <label for="remember">Remember me</label>
-                            </div>
-                            <a href="{{url('/password/reset')}}">Forgot password</a>
-                        </div>
-                        <button type="submit" class="butn btn-cta1b btn-lg">Sign In</button>
-                    </form>
-                    <div class="have-block">
-                        <span>Don’t have an account?</span>
-                        <a href="{{route('user.register.form')}}" class="sign-butn">Sign Up</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+ 
+  <div class="page-auth">
+      <div class="title-page">Sign In</div>
+      <div class="text-add">The easiest way for you to sign in is with Facebook</div>
+      <a href="" class="butn btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>Sign in with Facebook</a>
+      <div class="or-line upper">or</div>
+      <form action="{{route('user.login.post')}}" method="POST">
+          <div class="input-wrap">
+              <label>Enter your e-mail <span>*</span></label>
+              <input type="email" name="email" required>
+          </div>
+          <div class="input-wrap">
+              <label>Password <span>*</span></label>
+              <input type="password" name="password" required>
+          </div>
+          <div class="operations-pass">
+              <div class="input-wrap checkbox-wrap">
+                  <input id="remember" type="checkbox">
+                  <label for="remember">Remember me</label>
+              </div>
+              <a href="{{url('/password/reset')}}">Forgot password</a>
+          </div>
+          <button type="submit" class="butn btn-cta1b btn-lg">Sign In</button>
+      </form>
+      <div class="have-block">
+          <span>Don’t have an account?</span>
+          <a href="{{route('user.register.form')}}" class="sign-butn">Sign Up</a>
+      </div>
+  </div>
+ 
 @endsection
