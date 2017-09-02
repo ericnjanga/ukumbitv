3ab8af61b2e70eb4bdda7454b77cf1ebbc25338a
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-02 09:04:54 -0400

    Improving layout

--- resources/views/r/user/auth/login.blade.php
+++ resources/views/r/user/auth/login.blade.php
@@ -3,35 +3,40 @@
 
  
   <div class="page-auth">
-      <div class="text-center">
-      	<h1>{{trans('messages.auth_signin')}}</h1>
-	      <p>{{trans('messages.auth_signin_blurb')}}</p>
-	      <a href="" class="btn btn-block btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>{{trans('messages.auth_signin_fb')}}</a>
-      </div>
-      <div class="or-line upper">{{trans('messages.auth_or')}}</div>
-      <form action="{{route('user.login.post')}}" method="POST">
-          <div class="form-group">
-              <label for="email">{{trans('messages.auth_enter_email')}} <span>*</span></label>
-              <input class="form-control" type="email" name="email" required>
-          </div>
-          <div class="form-group">
-              <label for="password">{{trans('messages.auth_password')}} <span>*</span></label>
-              <input class="form-control" type="password" name="password" required>
-          </div>
-          <div class="checkbox">
-          	<label for="remember">
-          		<input id="remember" type="checkbox"> {{trans('messages.auth_remember')}}
-          	</label>
-	          <div class="block-forgot-pass">
-	          	<a href="{{url('/password/reset')}}">{{trans('messages.auth_forgot_password')}}</a>
-	          </div> 
-          </div>
-          <button type="submit" class="btn btn-block btn-cta1b btn-lg">{{trans('messages.auth_signin')}}</button>
-      </form>
-      <div class="have-block">
-          <span>{{trans('messages.auth_no_account')}}</span>
-          <a href="{{route('user.register.form')}}" class="sign-butn">Sign Up</a>
-      </div>
+    <div class="text-center">
+    	<h1>{{trans('messages.auth_signin')}}</h1>
+      <p>{{trans('messages.auth_signin_blurb')}}</p>
+      <a href="" class="btn btn-block btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>{{trans('messages.auth_signin_fb')}}</a>
+    </div>
+
+
+    <div class="or-line upper">{{trans('messages.auth_or')}}</div>
+
+
+    <form action="{{route('user.login.post')}}" method="POST">
+        <div class="form-group">
+            <label for="email">{{trans('messages.auth_enter_email')}} <span>*</span></label>
+            <input class="form-control" type="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="password">{{trans('messages.auth_password')}} <span>*</span></label>
+            <input class="form-control" type="password" name="password" required>
+        </div>
+        <div class="checkbox">
+        	<label for="remember">
+        		<input id="remember" type="checkbox"> {{trans('messages.auth_remember')}}
+        	</label>
+          <div class="block-forgot-pass">
+          	<a href="{{url('/password/reset')}}">{{trans('messages.auth_forgot_password')}}</a>
+          </div> 
+        </div>
+        <button type="submit" class="btn btn-block btn-cta1b btn-lg">{{trans('messages.auth_signin')}}</button>
+    </form>
+    
+    <div>
+        <span>{{trans('messages.auth_no_account')}}</span>
+        <a href="{{route('user.register.form')}}" class="sign-butn">{trans('messages.auth_signup')}}</a>
+    </div>
   </div>
  
 @endsection
--- resources/views/r/user/auth/register.blade.php
+++ resources/views/r/user/auth/register.blade.php
@@ -1,43 +1,43 @@
 @extends('r.layouts.simple')
 @section('content')
-    <div class="signup-wrap">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4">
-                    <div class="sign-block">
-                        <div class="title-page">Sign Up</div>
-                        <div class="text-add">The easiest way for you to sign up is with Facebook</div>
-                        <a href="" class="butn btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>Sign up with Facebook</a>
-                        <div class="or-line upper">or</div>
-                        <form  action="{{ url('/register') }}" method="POST">
-                            <div class="input-wrap">
-                                <label>Enter your e-mail <span>*</span></label>
-                                <input type="email" name="email" required>
-                            </div>
-                            <div class="input-wrap">
-                                <label>Password <span>*</span></label>
-                                <input type="password" name="password" class="mypass" required>
-                            </div>
-                            {{--<div class="operations-pass">--}}
-                                {{--<a href="">Show password</a>--}}
-                            {{--</div>--}}
-                            <div class="terms-policy">
-                                <p>By clicking Sign Up, I agree to the</p>
-                                <div>
-                                    <a href="{{route('user.terms-condition')}}">Terms of Service</a>
-                                    <span>and</span>
-                                    <a href="{{route('user.privacy_policy')}}">Privacy policy</a>
-                                </div>
-                            </div>
-                            <button type="submit" class="butn btn-cta1b btn-lg">Sign Up</button>
-                        </form>
-                        <div class="have-block">
-                            <span>Already have an account?</span>
-                            <a href="{{route('user.login.form')}}" class="sign-butn">Sign In</a>
-                        </div>
-                    </div>
-                </div>
+  <div class="page-auth">   
+    <div class="text-center">
+    	<h1>{{trans('messages.auth_signin')}}</h1>
+      <p>{{trans('messages.auth_signin_blurb')}}</p>
+      <a href="" class="btn btn-block btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>{{trans('messages.auth_signin_fb')}}</a>
+    </div>
+
+
+    <div class="or-line upper">{{trans('messages.auth_or')}}</div>
+
+
+    <form  action="{{ url('/register') }}" method="POST">
+        <div class="input-wrap">
+            <label>Enter your e-mail <span>*</span></label>
+            <input type="email" name="email" required>
+        </div>
+        <div class="input-wrap">
+            <label>Password <span>*</span></label>
+            <input type="password" name="password" class="mypass" required>
+        </div>
+        {{--<div class="operations-pass">--}}
+            {{--<a href="">Show password</a>--}}
+        {{--</div>--}}
+        <div class="terms-policy">
+            <p>By clicking Sign Up, I agree to the</p>
+            <div>
+                <a href="{{route('user.terms-condition')}}">Terms of Service</a>
+                <span>and</span>
+                <a href="{{route('user.privacy_policy')}}">Privacy policy</a>
             </div>
         </div>
+        <button type="submit" class="butn btn-cta1b btn-lg">Sign Up</button>
+    </form>
+    
+    <div>
+        <span>{{trans('messages.auth_have_account')}}</span>
+        <a href="{{route('user.login.form')}}" class="sign-butn">{{trans('messages.auth_signin')}}</a>
     </div>
+ 
+  </div>
 @endsection
