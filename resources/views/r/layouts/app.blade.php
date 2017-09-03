d88e34d70b3b535709ffad478527d636903b257d
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-03 13:16:20 -0400

    Improving layout

--- resources/views/r/layouts/app.blade.php
+++ resources/views/r/layouts/app.blade.php
@@ -28,6 +28,21 @@
     <link>
 </head>
 <body data-search-route="{{route('search-data')}}">
+ 
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1900426896906624";
+          fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+
+
+      
     @yield('layout')
 
 </body>
