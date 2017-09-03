dd220f00d2e11dc4e69786baf8c5415e603a3a37
Alex T <tgnwest@gmail.com>
2017-09-03 20:31:36 +0300

    add msg to movie list

--- resources/views/r/user/movie-list.blade.php
+++ resources/views/r/user/movie-list.blade.php
@@ -18,24 +18,28 @@
                         </div>
                         <div class="video-list-block-wrap">
                             <div class="video-list-block">
-                                @foreach($videos as $video)
-                                        <div class="video-item">
-                                            <a href="{{route('user.singleVideo',$video->watchid)}}">
-                                                <div class="video-img">
-                                                    <img src="{{$video->videoimage->imgPreview1}}" alt="">
-                                                </div>
-                                                <div class="video-title ellipsis-gradient">
-                                                    {{$video->title}}
-                                                </div>
-                                            </a>
-                                            <div class="video-info">
-                                                <div class="video-genre">{{$video->category->name}}</div>
-                                                <div class="butn-like"><span class="icon icon-thumbs-up"></span>
-                                                    <span class="likes-count">{{count($video->likes)}}</span>
+                                @if(count($videos) == 0)
+                                    <h1>{{trans('messages.There_is_no_videos')}}</h1>
+                                @else
+                                    @foreach($videos as $video)
+                                            <div class="video-item">
+                                                <a href="{{route('user.singleVideo',$video->watchid)}}">
+                                                    <div class="video-img">
+                                                        <img src="{{$video->videoimage->imgPreview1}}" alt="">
+                                                    </div>
+                                                    <div class="video-title ellipsis-gradient">
+                                                        {{$video->title}}
+                                                    </div>
+                                                </a>
+                                                <div class="video-info">
+                                                    <div class="video-genre">{{$video->category->name}}</div>
+                                                    <div class="butn-like"><span class="icon icon-thumbs-up"></span>
+                                                        <span class="likes-count">{{count($video->likes)}}</span>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div> 
-                                @endforeach
+                                    @endforeach
+                                @endif
                             </div>
                             <a href="" class="butn btn-cta1 btn-lg">Load more</a>
                         </div>
