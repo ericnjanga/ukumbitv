39175ec4d7fd0776bf5a037c962285e33afd96ea
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-06 19:54:38 -0400

    improved layout

--- resources/views/r/user/category-videos.blade.php
+++ resources/views/r/user/category-videos.blade.php
@@ -5,35 +5,7 @@
             <div class="clearfix">
                 @include('r.chunks._filter_video')
                 
-                <div class="global-main-content">
-                    <div class="block-wrap" style="display: none;">
-                        <div class="title">New Videos</div> 
-                        <div class="list-horizontal-wrapper">
-                            @for ($group=1;$group<3;$group++)
-                                @for($i=1; $i<5; $i++) 
-                                        <div class="video-item video-item-dis">
-                                            <a href="{{route('single-video',0)}}">
-                                                <div class="video-img">
-                                                    <img src="{{asset('r/img/video'.$i.'.png')}}" alt="">
-                                                </div>
-                                                <div class="video-title ellipsis-gradient">Transformers: Revenge of
-                                                    the
-                                                    Fallen
-                                                </div>
-                                            </a>
-                                            <div class="video-info">
-                                                <div class="video-genre">Drama</div>
-                                                <div class="butn-like"><span class="icon icon-thumbs-up"></span>125
-                                                </div>
-                                                <div class="butn-dis"><span
-                                                            class="icon icon-thumbs-down-hand"></span>19
-                                                </div>
-                                            </div>
-                                        </div> 
-                                @endfor
-                            @endfor
-                        </div> 
-                    </div>
+                <div class="global-main-content"> 
                     <div class="video-list-wrap">
                         <div class="title">All Videos</div>
                         <div class="filter-block-wrap">
@@ -63,7 +35,7 @@
                                         </div> 
                                    @endforeach
                             </div>
-                            <a href="" class="butn btn-cta1 btn-lg">Load more</a>
+                            <!-- <a href="" class="butn btn-cta1 btn-lg">Load more</a> -->
                         </div>
                     </div>
                 </div>
