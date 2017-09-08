6b50cdcce1b69cd0de3ed0b93aa657a1500f52f9
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-08 09:12:28 -0400

    improving layout

--- resources/views/r/user/packages.blade.php
+++ resources/views/r/user/packages.blade.php
@@ -74,52 +74,6 @@
 
 
 
-      <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingOne">
-      <h4 class="panel-title">
-        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </a>
-      </h4>
-    </div>
-    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingTwo">
-      <h4 class="panel-title">
-        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
-        </a>
-      </h4>
-    </div>
-    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingThree">
-      <h4 class="panel-title">
-        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </a>
-      </h4>
-    </div>
-    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
-
-
 
 
 
@@ -136,135 +90,163 @@
     	<!-- payment info section --> 
       <section class="section-payment">
         <h1>Payment information</h1>
-        <div class="payment-info--current">
+
+	      <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+				  <div class="panel panel-default panel-payinfo">
+				    <div class="panel-heading" role="tab" id="headingOne">
+				      <h4 class="panel-title">
+				        <a role="button" class="btn btn-cta1b btn-lg open" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+				          Update Payment Information
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne"> 		        <div class="panel-body payment-content">
+		            <h2>Update Payment Information</h2>
+		            <p class="payment-text">Your new payment method will be applied to your next billing cycle. Your monthly membership is billed on the first day of each billing period.</p>
+		            <!-- <div class="card-text">
+		                <span class="bold">Credit or Debit Card</span>
+		                <img src="{{asset('r/img/visa.png')}}" alt="">
+		                <img src="{{asset('r/img/mastercard.png')}}" alt="">
+		            </div> -->
+		            <div class="payment-form-block">
+		              <form action="" method="" class="xl-inputs"> 
+		              	<div class="row">
+		              		<div class="col-md-12"> 
+				                <div class="input-group">
+				                  <label>Card Number</label>
+				                  <input type="text" name="cardnumber" class="form-control">
+				                  <div class="card-samples">
+				                  	<div id="card-image-container-Visa" role="widget" aria-live="polite" name="card-image-creditCardType" class="card-image-Visa-disabled card-image " aria-selected="false" aria-hidden="true" aria-disabled="true">
+								              <span id="card-image-text-Visa" class="card-image-text-Visa sr-only">
+								                Visa 
+								              </span>
+								           </div><!-- Visa -->
+								           <div id="card-image-container-MasterCard" role="widget" aria-live="polite" name="card-image-creditCardType" class="card-image-MasterCard-disabled card-image " aria-selected="false" aria-hidden="true" aria-disabled="true">
+								              <span id="card-image-text-MasterCard" class="card-image-text-MasterCard sr-only">
+								                MasterCard 
+								              </span>
+								           </div><!-- MasterCard -->
+				                  </div><!-- card samples -->
+				                </div>
+		              		</div><!-- col -->
+		              	</div><!-- row -->
+
+		              	<div class="row">
+		              		<div class="col-md-4"> 
+				                <div class="input-group">
+				                  <label>Expiration (MM)</label>
+				                  <select name="expiry-month" id="expiry-month" class="form-control">
+				                  	<option value="">Month</option>
+				                  	<!-- option list -->
+				                  	<!-- All 12 months -->
+				                  </select> 
+				                </div><!-- month -->
+		              		</div><!-- col -->
+
+		              		<div class="col-md-4"> 
+				                <div class="input-group">
+				                  <label>Expiration (YYYY)</label>
+				                  <select name="expiry-year" id="expiry-year" class="form-control">
+				                  	<option value="">Year</option> 
+				                  	<!-- option list -->
+				                  	<!-- from current year up to 30 years -->
+				                  </select> 
+				                </div><!-- year -->
+		              		</div><!-- col -->
+
+		              		<div class="col-md-4"> 
+				                <div class="input-group">
+				                  <label>CVV</label>
+		                      <input type="text" name="cvv" id="cvv" class="form-control"> 
+				                </div><!-- year -->
+		              		</div><!-- col -->
+		              	</div><!-- row -->
+		              	
+		              	<div class="row">
+		              		<div class="col-md-12"> 
+				                <div class="input-group">
+		                      <label>Cardholder Name</label>
+		                      <input type="text" name="cardhlder-name" class="form-control">
+				                </div>
+		              		</div><!-- col -->
+		              	</div><!-- row -->
+		              	
+		              	<div class="row">
+		              		<div class="col-md-12"> 
+				                <div class="input-group">
+				                  <label>Country</label>
+				                  <select name="expiry-year" id="expiry-year" class="form-control">
+				                  	<option value="">Select a country</option> 
+				                  	<!-- option list -->
+				                  	<!-- from current year up to 30 years -->
+				                  </select> 
+				                </div>
+		              		</div><!-- col -->
+		              	</div><!-- row -->
+		              	
+		              	<div class="row">
+		              		<div class="col-md-12"> 
+				                <div class="input-group">
+		                      <label>State/Province/Region</label>
+		                      <input type="text" name="cardhlder-state" class="form-control">
+				                </div>
+		              		</div><!-- col -->
+		              	</div><!-- row -->
+		              	
+		              	<div class="row">
+		              		<div class="col-md-12"> 
+				                <div class="input-group">
+		                      <label>Zip/Postal Code</label>
+		                      <input type="text" name="cardhlder-zip" class="form-control">
+				                </div>
+		              		</div><!-- col -->
+		              	</div><!-- row -->
+		 
+		                <div class="row section-submit">
+		                	<div class="col-md-12">
+		                		<button type="submit" class="btn btn-primary btn-block btn-lg">Update payment method</button>
+		                	</div><!-- col -->  
+		                </div><!-- row -->
+		              </form>
+		            </div>
+		          </div><!-- panel-body payment-content --> 
+				    </div><!-- collapseOne -->
+				  </div><!-- panel-payinfo -->
+				  <div class="panel panel-default panel-paypal">
+				    <div class="panel-heading" role="tab" id="headingTwo">
+				      <h4 class="panel-title">
+				        <a class="collapsed" class="btn btn-cta1b btn-lg open" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+				          Paypal
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo"> 
+			        <div class="panel-body paypal-block">
+	              <div class="card-text">
+	                  <span class="bold">Pay Pal</span>
+	                  <img src="{{asset('r/img/paypal.png')}}" alt="">
+	              </div>
+	              <p class="payment-text">To finish sign-up, click on the "Continue to PayPal" button and log on to PayPal using your email and password.</p>
+	              <a href="" class="btn btn-primary btn-lg">Continue to Pay Pal</a>
+		          </div><!-- panel-body paypal-block -->  
+				    </div><!-- collapseTwo -->
+				  </div><!-- panel-paypal -->
+				</div><!-- panel-group -->
+
+
+
+        <!-- <div class="payment-info--current">
             <div class="payment-info"><span class="icon icon-paypal-big-logo"></span>example@redentu.com</div>
             <a id="payinfo" href="" class="btn btn-cta1b btn-lg">Update payment information <span class="icon icon-scroll-arrow-to-down"></span></a>
-        </div>
-        <div class="payment-info--edit">
-          <div class="payment-content">
-            <h2>Update Payment Information</h2>
-            <p class="payment-text">Your new payment method will be applied to your next billing cycle. Your monthly membership is billed on the first day of each billing period.</p>
-            <!-- <div class="card-text">
-                <span class="bold">Credit or Debit Card</span>
-                <img src="{{asset('r/img/visa.png')}}" alt="">
-                <img src="{{asset('r/img/mastercard.png')}}" alt="">
-            </div> -->
-            <div class="payment-form-block">
-              <form action="" method="" class="xl-inputs"> 
-              	<div class="row">
-              		<div class="col-md-12"> 
-		                <div class="input-group">
-		                  <label>Card Number</label>
-		                  <input type="text" name="cardnumber" class="form-control">
-		                  <div class="card-samples">
-		                  	<div id="card-image-container-Visa" role="widget" aria-live="polite" name="card-image-creditCardType" class="card-image-Visa-disabled card-image " aria-selected="false" aria-hidden="true" aria-disabled="true">
-						              <span id="card-image-text-Visa" class="card-image-text-Visa sr-only">
-						                Visa 
-						              </span>
-						           </div><!-- Visa -->
-						           <div id="card-image-container-MasterCard" role="widget" aria-live="polite" name="card-image-creditCardType" class="card-image-MasterCard-disabled card-image " aria-selected="false" aria-hidden="true" aria-disabled="true">
-						              <span id="card-image-text-MasterCard" class="card-image-text-MasterCard sr-only">
-						                MasterCard 
-						              </span>
-						           </div><!-- MasterCard -->
-		                  </div><!-- card samples -->
-		                </div>
-              		</div><!-- col -->
-              	</div><!-- row -->
-
-              	<div class="row">
-              		<div class="col-md-4"> 
-		                <div class="input-group">
-		                  <label>Expiration (MM)</label>
-		                  <select name="expiry-month" id="expiry-month" class="form-control">
-		                  	<option value="">Month</option>
-		                  	<!-- option list -->
-		                  	<!-- All 12 months -->
-		                  </select> 
-		                </div><!-- month -->
-              		</div><!-- col -->
-
-              		<div class="col-md-4"> 
-		                <div class="input-group">
-		                  <label>Expiration (YYYY)</label>
-		                  <select name="expiry-year" id="expiry-year" class="form-control">
-		                  	<option value="">Year</option> 
-		                  	<!-- option list -->
-		                  	<!-- from current year up to 30 years -->
-		                  </select> 
-		                </div><!-- year -->
-              		</div><!-- col -->
-
-              		<div class="col-md-4"> 
-		                <div class="input-group">
-		                  <label>CVV</label>
-                      <input type="text" name="cvv" id="cvv" class="form-control"> 
-		                </div><!-- year -->
-              		</div><!-- col -->
-              	</div><!-- row -->
-              	
-              	<div class="row">
-              		<div class="col-md-12"> 
-		                <div class="input-group">
-                      <label>Cardholder Name</label>
-                      <input type="text" name="cardhlder-name" class="form-control">
-		                </div>
-              		</div><!-- col -->
-              	</div><!-- row -->
-              	
-              	<div class="row">
-              		<div class="col-md-12"> 
-		                <div class="input-group">
-		                  <label>Country</label>
-		                  <select name="expiry-year" id="expiry-year" class="form-control">
-		                  	<option value="">Select a country</option> 
-		                  	<!-- option list -->
-		                  	<!-- from current year up to 30 years -->
-		                  </select> 
-		                </div>
-              		</div><!-- col -->
-              	</div><!-- row -->
-              	
-              	<div class="row">
-              		<div class="col-md-12"> 
-		                <div class="input-group">
-                      <label>State/Province/Region</label>
-                      <input type="text" name="cardhlder-state" class="form-control">
-		                </div>
-              		</div><!-- col -->
-              	</div><!-- row -->
-              	
-              	<div class="row">
-              		<div class="col-md-12"> 
-		                <div class="input-group">
-                      <label>Zip/Postal Code</label>
-                      <input type="text" name="cardhlder-zip" class="form-control">
-		                </div>
-              		</div><!-- col -->
-              	</div><!-- row -->
- 
-                <div class="row section-submit">
-                	<div class="col-md-12">
-                		<button type="submit" class="btn btn-primary btn-block btn-lg">Update payment method</button>
-                	</div><!-- col -->  
-                </div><!-- row -->
-              </form>
-            </div>
-          </div>
+        </div> -->
+        <!-- <div class="payment-info--edit">
+          
 
           <hr>
 
-          <div class="paypal-block">
-              <div class="card-text">
-                  <span class="bold">Pay Pal</span>
-                  <img src="{{asset('r/img/paypal.png')}}" alt="">
-              </div>
-              <p class="payment-text">To finish sign-up, click on the "Continue to PayPal" button and log on to PayPal using your email and password.</p>
-              <a href="" class="btn btn-primary btn-lg">Continue to Pay Pal</a>
-          </div>
-        </div>
-      </section> 
-    	<!-- payment info section -->
+          
+        </div> -->
+      </section> <!-- section-payment --> 
     </div><!-- "global-main-content -->
   </div>
 @endsection
