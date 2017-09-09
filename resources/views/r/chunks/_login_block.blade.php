9c7aa8d73932145cf9f2ed0dd20cf0acf69401c3
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-09 10:04:15 -0400

    improving layout

--- resources/views/r/chunks/_login_block.blade.php
+++ resources/views/r/chunks/_login_block.blade.php
@@ -1,3 +1,25 @@
+<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+
+  <span class="nav-user-access">
+  	<span class="login-info">
+      <span class="login-text">@if(isset($videoCount)){{$videoCount}} videos left @else Unlimited videos @endif</span>
+      <a href="{{route('user.package')}}" class="login-upgrade">upgrade package</a>
+  	</span> 
+		<img src="{{asset('r/img/no-img.png')}}" alt="">
+  </span>
+	<span class="caret"></span>
+</a>
+<ul class="dropdown-menu">
+  <li><a href="#">Action</a></li>
+  <li><a href="#">Another action</a></li>
+  <li><a href="#">Something else here</a></li>
+  <li role="separator" class="divider"></li>
+  <li><a href="#">Separated link</a></li>
+</ul>
+
+
+
+<!-- 
 <div class="login-block">
     <div class="login-info">
         <div class="login-text">@if(isset($videoCount)){{$videoCount}} videos left @else Unlimited videos @endif</div>
@@ -13,4 +35,4 @@
             </ul>
         </div>
     </div>
-</div>
+</div> -->
