ee288dd829cc1850f3b33e68351fa9926280df3c
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-10 00:37:32 -0400

    improving layout

--- resources/views/r/user/category-videos.blade.php
+++ resources/views/r/user/category-videos.blade.php
@@ -7,14 +7,17 @@
     	<div class="global-content">
 	      <div class="video-list-wrap">
           <div class="title">All Videos</div>
-          <div class="filter-block-wrap">
+
+          <!-- for step 2 -->
+          <!-- for step 2 -->
+          <!-- <div class="filter-block-wrap">
             <div class="filter-block">
               @include('r.chunks._sort_by')
             </div>
             <div class="filter-block">
               @include('r.chunks._filter_tags')
             </div>
-          </div>
+          </div> -->
            
           <div class="block-wrap">
               @foreach($videos as $video)
