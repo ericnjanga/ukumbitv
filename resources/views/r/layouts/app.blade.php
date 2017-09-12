792cd6450c132237f040b2c5132165232d8072ba
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-12 12:56:04 -0400

    improving layout

--- resources/views/r/layouts/app.blade.php
+++ resources/views/r/layouts/app.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{csrf_token()}}">
-    <title>UkumbiTV | {{trans('messages.home_midsec_title1')}</title>
+    <title>UkumbiTV | {{trans('messages.home_midsec_title1')}}</title>
   @if(isset($video))
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="UkumbiTV" />
