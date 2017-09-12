dd7b118889bfe816ab1a521da217ecc2737de9f5
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2017-09-12 13:14:44 -0400

    adding google analytics code

--- resources/views/r/layouts/app.blade.php
+++ resources/views/r/layouts/app.blade.php
@@ -34,7 +34,7 @@
 
 </head>
 <body data-search-route="{{route('search-data')}}">
- 
+ 	{{--FACEBOOK CODE--}}
   <div id="fb-root"></div>
   <script>(function(d, s, id) {
           var js, fjs = d.getElementsByTagName(s)[0];
@@ -44,7 +44,17 @@
           fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));</script>
 
+ 	{{--GOOGLE ANALYTICS CODE--}}
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+	  ga('create', 'UA-106334552-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 
 
 
