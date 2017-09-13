6ee2f30582b1e5fcc1889ca96a3c93cf29cd8270
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-13 18:56:10 -0400

    enabling form validation

--- resources/views/r/user/auth/passwords/email.blade.php
+++ resources/views/r/user/auth/passwords/email.blade.php
@@ -1,26 +1,26 @@
 @extends('r.layouts.simple')
 @section('content')
-    <div class="resetpas-wrap">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3">
-                    <div class="title-page">Reset password</div>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4">
-                    <div class="notice-block">
-                        <div class="text-add">We will send new password on your e-mail</div>
-                        <form  action="" method="">
-                            <div class="input-wrap">
-                                <label>E-mail <span>*</span></label>
-                                <input type="email" name="email" required>
-                            </div>
-                            <button type="submit" class="butn btn-cta1b btn-lg">Reset password</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+ 
+  <div class="page-auth" ng-app="validationApp" ng-controller="mainController">
+    <h1 class="text-center">Reset password</h1>
+
+    <p>We will send new password on your e-mail</p>
+    <form name="recoverForm" action="" method="">
+
+
+      <div class="form-group" ng-class="{ 'has-error' : recoverForm.email.$invalid && !recoverForm.email.$pristine }">
+        <label for="email">{{trans('messages.auth_enter_email')}} <span>*</span></label>
+        <input class="form-control" type="email" name="email" ng-model="user.email" required>
+        <div ng-cloak ng-show="recoverForm.email.$invalid && !recoverForm.email.$pristine" class="help-block">Enter a valid email.</div> 
+      </div>
+ 
+      <button type="submit" class="butn btn-cta1b btn-lg" ng-disabled="loginForm.$invalid">Reset password</button>
+    </form>
+	</div>
+
+
+    
+        
+    
 @endsection
