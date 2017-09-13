25af1bb811beffd4e115bb866170cdbccc226745
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-13 18:09:04 -0400

    enabling form validation

--- resources/views/r/user/auth/login.blade.php
+++ resources/views/r/user/auth/login.blade.php
@@ -15,7 +15,7 @@
 
     <form name="loginForm" action="{{route('user.login.post')}}" method="POST" autocomplete="off" novalidate>
 
-			{!! ---Email Address--- !!}
+			<!-- Email Address -->
       <div class="form-group" ng-class="{ 'has-error' : loginForm.email.$invalid && !loginForm.email.$pristine }">
         <label for="email">{{trans('messages.auth_enter_email')}} <span>*</span></label>
         <input class="form-control" type="email" name="email" ng-model="user.email" required>
@@ -24,13 +24,13 @@
 
 
       <!-- Password -->
-     <!--  <div class="form-group" ng-class="{ 'has-error' : loginForm.password.$invalid && !loginForm.password.$pristine }">
+      <div class="form-group" ng-class="{ 'has-error' : loginForm.password.$invalid && !loginForm.password.$pristine }">
         <label>Password</label>   
         <input type="password" name="password" class="form-control" ng-model="user.password" ng-minlength="6" required>
         <p ng-show="loginForm.password.$dirty && loginForm.password.$viewValue.length==0" class="help-block">You password is required.</p> 
         <p ng-show="loginForm.password.$error.minlength" class="help-block">Password is too short.</p>
         <p ng-show="loginForm.password.$error.pattern" class="help-block">Your assword should contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character.</p>
-    </div> -->
+    </div>
       
 
 
