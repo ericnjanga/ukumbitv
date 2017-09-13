c35edfc8fcf0e748c58286ae7cc6a6658ef43de6
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-12 22:04:09 -0400

    improving layout

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -852,8 +852,10 @@ return array(
   //WELCOME EMAIL ...
   //-----------------------------------  
   'welcome_email_title' => 'Welcome to UkumbiTV',
-  'welcome_email_cantread1' => 'If you can\'t see this message,',
-  'welcome_email_cantread2' => 'If you can\'t see this message,',
+  'welcome_email_cantread1' => 'If you can\'t see this message',
+  'welcome_email_cantread2' => 'view&nbsp;it&nbsp;in&nbsp;your&nbsp;browser',
+  'welcome_email_H1' => 'Welcome to UkumbiTV',
+  'welcome_email_H2' => 'Thank You for registering',
 
 
  
