75aa6aa6bf059b4d4b43617bd3505d4df81c762a
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-13 18:40:08 -0400

    enabling form validation

--- resources/views/r/user/auth/register.blade.php
+++ resources/views/r/user/auth/register.blade.php
@@ -11,7 +11,7 @@
     <div class="or-line upper">{{trans('messages.auth_or')}}</div>
 
 
-    <form  action="{{ url('/register') }}" method="POST" autocomplete="off" novalidate>
+    <form name="registerForm" action="{{ url('/register') }}" method="POST" autocomplete="off" novalidate>
       @if($errors->has('email') || $errors->has('password_confirmation'))
         <div data-abide-error="" class="alert callout">
           <p>
@@ -32,40 +32,40 @@
  
  
 			<!-- Email Address -->
-      <div class="form-group" ng-class="{ 'has-error' : loginForm.email.$invalid && !loginForm.email.$pristine }">
+      <div class="form-group" ng-class="{ 'has-error' : registerForm.email.$invalid && !registerForm.email.$pristine }">
         <label for="email">{{trans('messages.auth_enter_email')}} <span>*</span></label>
         <input class="form-control" type="email" name="email" ng-model="user.email" required>
-        <div ng-cloak ng-show="loginForm.email.$invalid && !loginForm.email.$pristine" class="help-block">Enter a valid email.</div> 
+        <div ng-cloak ng-show="registerForm.email.$invalid && !registerForm.email.$pristine" class="help-block">Enter a valid email.</div> 
       </div>
 
 
       <!-- Password -->
-      <div class="form-group" ng-class="{ 'has-error' : loginForm.password.$invalid && !loginForm.password.$pristine }">
+      <div class="form-group" ng-class="{ 'has-error' : registerForm.password.$invalid && !registerForm.password.$pristine }">
         <label>Password</label>   
         <input type="password" name="password" class="form-control" ng-model="user.password" ng-minlength="6" required>
-        <div ng-cloak ng-show="loginForm.password.$dirty && loginForm.password.$viewValue.length==0" class="help-block">You password is required.</div> 
-        <div ng-cloak ng-show="loginForm.password.$error.minlength" class="help-block">Password is too short.</div>
-        <div ng-cloak ng-show="loginForm.password.$error.pattern" class="help-block">Your assword should contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character.</div>
+        <div ng-cloak ng-show="registerForm.password.$dirty && registerForm.password.$viewValue.length==0" class="help-block">You password is required.</div> 
+        <div ng-cloak ng-show="registerForm.password.$error.minlength" class="help-block">Password is too short.</div>
+        <div ng-cloak ng-show="registerForm.password.$error.pattern" class="help-block">Your assword should contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character.</div>
     	</div> 
 
  
       
       <!-- Password Confirmation -->
-      <div class="form-group" ng-class="{ 'has-error' : (loginForm.password.$valid && loginForm.confirmPassword.$dirty &&loginForm.confirmPassword.$error.passwordMatch) }">
+      <div class="form-group" ng-class="{ 'has-error' : (registerForm.password.$valid && registerForm.confirmPassword.$dirty &&registerForm.confirmPassword.$error.passwordMatch) }">
         <label>Re-Type New Password</label> 
       
 				<input type="password" match-password="password" name="confirmPassword" ng-model="user.confirmPassword" class="form-control" required>
-        <p ng-show="(loginForm.password.$valid && loginForm.confirmPassword.$dirty &&loginForm.confirmPassword.$error.passwordMatch)" class="help-block">Password doesn't match.</p> 
+        <p ng-show="(registerForm.password.$valid && registerForm.confirmPassword.$dirty &&registerForm.confirmPassword.$error.passwordMatch)" class="help-block">Password doesn't match.</p> 
       </div>
  
 
       <p class="fine-print">{{trans('messages.auth_policy_check1')}} <a href="{{route('user.terms-condition')}}">{{trans('messages.tos_title')}}</a> <span>{{trans('messages.and')}}</span> <a href="{{route('user.privacy_policy')}}">{{trans('messages.PP_title')}}</a></p> 
 
-      <button type="submit" class="btn btn-block btn-cta1b btn-lg" ng-disabled="loginForm.$invalid">{{trans('messages.auth_signup')}}</button>
+      <button type="submit" class="btn btn-block btn-cta1b btn-lg" ng-disabled="registerForm.$invalid">{{trans('messages.auth_signup')}}</button>
     </form>
 
 
-    
+
     
     <div>
       <span>{{trans('messages.auth_have_account')}}</span>
