b1ebb4296a399d64aa4bf2a0cd849a68aae28f2b
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-12 23:14:07 -0400

    improving layout

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -485,43 +485,7 @@
 								</td>
 							</tr>
 							<!-- // MODULE ROW -->
-
-    
-
-							<!-- MODULE ROW // -->
-							<tr>
-								<td align="center" valign="top">
-									<!-- CENTERING TABLE // -->
-									<table border="0" cellpadding="0" cellspacing="0" width="100%">
-										<tr>
-											<td align="center" valign="top">
-												<!-- FLEXIBLE CONTAINER // -->
-												<table border="0" cellpadding="30" cellspacing="0" width="500" class="flexibleContainer">
-													<tr>
-														<td style="padding-top:0;" align="center" valign="top" width="500" class="flexibleContainerCell">
-
-															<!-- CONTENT TABLE // -->
-															<table align="left" border="0" cellpadding="0" cellspacing="0" class="flexibleContainer">
-																<tr>
-																	<td align="left" valign="top" class="textContent">
-																		<div style="text-align:left;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;margin-top:10px;color:#5F5F5F;line-height:135%;">Pellentesque habitant morbi tristique senectus netus malesuada et fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</div>
-																	</td>
-																</tr>
-															</table>
-															<!-- // CONTENT TABLE -->
-
-														</td>
-													</tr>
-												</table>
-												<!-- // FLEXIBLE CONTAINER -->
-											</td>
-										</tr>
-									</table>
-									<!-- // CENTERING TABLE -->
-								</td>
-							</tr>
-							<!-- // MODULE ROW -->
-
+ 
 
 							<!-- MODULE ROW // -->
 							<tr>
