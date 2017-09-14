77c221d2761ec80a0923193b2dd4d06486677b79
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-14 04:29:20 -0400

    improving site html semantic

--- resources/views/r/landing.blade.php
+++ resources/views/r/landing.blade.php
@@ -45,25 +45,25 @@
           	<h2 class="title text-center">{{trans('messages.home_plansec_title')}}</h2>
             <div class="price-block">
               @foreach($payment_plans as $payment_plan)
-                  <div class="price-item">
-                    <div class="price-title">{{$payment_plan->name}}</div>
-                    <div class="count-text">{{$payment_plan->description}}</div>
-                    <ul class="includ-list">
-                      @php($i=1)
-                      @while(isset($payment_plan->{'product'.$i}))
-                          <li>{!! $payment_plan->{'product'.$i} !!}</li>
-                          @php($i++)
-                      @endwhile
-                    </ul>
-                    <!-- <div class="price upper">{{$payment_plan->price == '0'?'Free':'$ '.$payment_plan->price}}</div> -->
-                    
-										@if($payment_plan->price == '0') 
-											<!-- <div class="price upper">{{trans('messages.free')}}</div> -->
-											<a href="{{route('user.register.form')}}" class="btn btn-cta1b btn-lg">{{trans('messages.home_cta')}}</a> 
-										@else 
-											<div class="price upper">{{$payment_plan->price}}<small>/{{trans('messages.month')}}</small></div>
-										@endif 
-                  </div>
+                <section class="price-item">
+                  <div class="price-title">{{$payment_plan->name}}</div>
+                  <div class="count-text">{{$payment_plan->description}}</div>
+                  <ul class="includ-list">
+                    @php($i=1)
+                    @while(isset($payment_plan->{'product'.$i}))
+                        <li>{!! $payment_plan->{'product'.$i} !!}</li>
+                        @php($i++)
+                    @endwhile
+                  </ul>
+                  <!-- <div class="price upper">{{$payment_plan->price == '0'?'Free':'$ '.$payment_plan->price}}</div> -->
+                  
+									@if($payment_plan->price == '0') 
+										<!-- <div class="price upper">{{trans('messages.free')}}</div> -->
+										<a href="{{route('user.register.form')}}" class="btn btn-cta1b btn-lg">{{trans('messages.home_cta')}}</a> 
+									@else 
+										<div class="price upper">{{$payment_plan->price}}<small>/{{trans('messages.month')}}</small></div>
+									@endif 
+                </section>
               @endforeach 
             </div> 
           </div>
@@ -81,10 +81,10 @@
     	<a class="link-inner-redirect" name="section3"></a> 
       <div class="container"> 
         <div class="row">
-          <div class="col-md-6 col-md-offset-3">
+          <article class="col-md-6 col-md-offset-3">
           	<h2 class="title text-center">{{trans('messages.home_seotext_title')}}</h2>
               {!!trans('messages.home_seotext_blurb')!!}
-          </div>
+          </article>
         </div>
       </div>
     </section><!-- section -->
