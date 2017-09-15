ca52b94963f9cb6f559d821014274fea2a98b4ca
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-15 10:59:59 -0400

    optimizing website

--- resources/views/r/layouts/app.blade.php
+++ resources/views/r/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
     <link rel="stylesheet" href="{{asset('r/css/style.css')}}">
-    <!-- <link rel="stylesheet" href="{{asset('r/css/media.css')}}"> -->
+    <link rel="stylesheet" href="{{asset('r/css/style.css.map')}}">
     <link rel="stylesheet" href="{{asset('packages/sweetalert/sweetalert.css')}}"> 
     <link rel="stylesheet" href="{{asset('r/css/passfield.min.css')}}">
 		
