1cabae06dfbadf21f49d59e3da4ccee90d91d845
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-14 22:48:28 -0400

    improving layout

--- resources/views/r/user/packages.blade.php
+++ resources/views/r/user/packages.blade.php
@@ -11,7 +11,7 @@
 	      		<h1>
 							<span class="badge">1</span>
 	        	{{trans('messages.home_plansec_title')}}</h1>
-	        	<p>Start by picking the right package for you. Then choose a payment method.</p>
+	        	<p>To activate a plan, start by picking a package, then choose a payment method.</p>
 	      	</header>
 	        
 			  @if(Session::has('flash_success'))
