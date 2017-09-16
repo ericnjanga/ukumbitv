8837a97440d2efdf70bd54dce982a84033f3b20e
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-16 13:58:13 -0400

    improving layout

--- resources/views/r/static/about-us.blade.php
+++ resources/views/r/static/about-us.blade.php
@@ -14,13 +14,22 @@
 	context and without any fear nor doubt, « Ukumbi TV » after a great reflection is just the perfect brand's name.</p>
 	      	</article>
 	      	<div class="col-md-4">
-	      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore at, quasi officiis vero distinctio voluptatibus dolor, dicta a repellendus ea soluta et obcaecati aut veniam. Numquam id ipsa quam odit.</p>
+	      		<h2>ORIGINS</h2>
+	      		<h3>The rise of African independent filmakers</h3>
+	      		<p>The presence of video streaming platforms such as YouTube witnessed the evolution of a great number of small and independent movie production companies in Africa, this made it advantegeous for African film producers to show case their productions on the web at a low cost rate for the pleasure of millions of viewers.</p>
 	      	</div>
 	      	<div class="col-md-4">
-	      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore at, quasi officiis vero distinctio voluptatibus dolor, dicta a repellendus ea soluta et obcaecati aut veniam. Numquam id ipsa quam odit.</p>
+	      		<h3>The Problem : Limited regulation, Little progress</h3>
+	      		<p>The film industry in some African countries is still undesirable and less regulated. This turns to obstruct it from reaching international standards thereby reducing it's chances to compete with other industries world wide. It has had a great negative impact on their productions, and has hinder local talents by preventing them from making a living out of their art.</p>
 	      	</div>
 	      	<div class="col-md-4">
-	      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore at, quasi officiis vero distinctio voluptatibus dolor, dicta a repellendus ea soluta et obcaecati aut veniam. Numquam id ipsa quam odit.</p>
+	      		<h3>The solution : Why not do it like Netflix ?</h3>
+	      		<p>It is not a secret and will neither be an exageration to say that Netflix has successfully brought back to life the home television business by creating a multi-billion dollar film industry, firstly by streaming Hollywood movies and secondly by indulging herself in her own original movies production. Ukumbi TV dreams same for the African cinema. We are taking a bold step ahead in nourishing local talented actors and producers.</p>
+	      		<ul>
+	      			<li>Firstly,Ukumbi TV streams African movies locally made by talented actors and producers of the continent.</li>
+	      			<li>The initial benefits will be re-invested in the production of the brand's own original movies.</li>
+	      			<li>From our benefits, we shall proceed into the establishment of local art schools, inorder to elevate the huge local talents Africa has and to further improve the quality so as to make the African cinema meet up competetively with international standards of film making.</li>
+	      		</ul>
 	      	</div>
 	      	<div class="col-md-4">
 	      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore at, quasi officiis vero distinctio voluptatibus dolor, dicta a repellendus ea soluta et obcaecati aut veniam. Numquam id ipsa quam odit.</p>
