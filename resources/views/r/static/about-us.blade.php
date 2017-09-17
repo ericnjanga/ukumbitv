3c8b69b6ae9cb4ab0fcc82a593ec2b179c79a5b8
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-16 21:19:21 -0400

    updating about page

--- resources/views/r/static/about-us.blade.php
+++ resources/views/r/static/about-us.blade.php
@@ -7,45 +7,27 @@
 	        <h1>About us</h1> 
 	        <p>At UkumbiTV, we're passionate about developing the African cinema to the highest international standards!</p>
 	      </header>
-      	<article>
-      		<!-- <article class="col-md-4"> -->
+      	<article> 
 	      		<h2>The Brand</h2>
 	      		<p>Drawn from her Swahilian origin « theatre », Ukumbi signifies « the show happening on the user's device ». Our main focus here is to embrace the Africain cultural heritage through our brand in its modern
-	context and without any fear nor doubt, « Ukumbi TV » after a great reflection is just the perfect brand's name.</p>
-	      	<!-- </article> -->
-	      	<!-- <div class="col-md-4"> -->
+	context and without any fear nor doubt, « UkumbiTV » after a great reflection is just the perfect brand's name.</p>
+	      	 
 	      		<h2>ORIGINS</h2>
 	      		<h3>The rise of African independent filmakers</h3>
-	      		<p>The presence of video streaming platforms such as YouTube witnessed the evolution of a great number of small and independent movie production companies in Africa, this made it advantegeous for African film producers to show case their productions on the web at a low cost rate for the pleasure of millions of viewers.</p>
-	      	<!-- </div>
-	      	<div class="col-md-4"> -->
+	      		<p>The presence of video streaming platforms such as YouTube witnessed the evolution of a great number of small and independent movie production companies in Africa, this made it advantegeous for African film producers to showcase their productions on the web at a low cost rate for the pleasure of millions of viewers.</p>
+	      
 	      		<h3>The Problem : Limited regulation, Little progress</h3>
 	      		<p>The film industry in some African countries is still undesirable and less regulated. This turns to obstruct it from reaching international standards thereby reducing it's chances to compete with other industries world wide. It has had a great negative impact on their productions, and has hinder local talents by preventing them from making a living out of their art.</p>
 	      	
 	      		<h3>The solution : Why not do it like Netflix ?</h3>
-	      		<p>It is not a secret and will neither be an exageration to say that Netflix has successfully brought back to life the home television business by creating a multi-billion dollar film industry, firstly by streaming Hollywood movies and secondly by indulging herself in her own original movies production. Ukumbi TV dreams same for the African cinema. We are taking a bold step ahead in nourishing local talented actors and producers.</p>
+	      		<p>It is not a secret and will neither be an exageration to say that Netflix has successfully reinvented the home television business by creating a multi-billion dollar film industry, firstly by streaming Hollywood movies and secondly by indulging herself in her own original movies production. UkumbiTV dreams same for the African cinema. We are taking a bold step ahead in nourishing local talented actors and producers:</p>
 	      		<ul class="list-decimal">
-	      			<li>Firstly,Ukumbi TV streams African movies locally made by talented actors and producers of the continent.</li>
-	      			<li>The initial benefits will be re-invested in the production of the brand's own original movies.</li>
+	      			<li>Firstly, UkumbiTV streams African movies locally made by talented actors and producers of the continent</li>
+	      			<li>The initial benefits will be re-invested in the production of the brand's own original movies</li>
 	      			<li>From our benefits, we shall proceed into the establishment of local art schools, inorder to elevate the huge local talents Africa has and to further improve the quality so as to make the African cinema meet up competetively with international standards of film making.</li>
 	      		</ul>
 	      	
-      	</article><!-- row -->
-      	<!-- 
-        <div class="col-sm-12 col-md-9 col-lg-7 col-xl-6">
-          <div class="content-block">
-          	 
-            <div class="img-block">
-                <img src="{{asset('r/img/landing-bg.png')}}" alt="">
-            </div>
-             
-            <div class="content-block-bottom">
-                <div class="title-page-add">Questions ?</div>
-                <div class="text-add">Fill free to contact us</div>
-                <a href="{{route('user.contact')}}" class="butn btn-cta1b btn-lg">Contact us</a>
-            </div>
-          </div>
-        </div> -->
+      	</article><!-- row --> 
       </section>
 
       <hr>
