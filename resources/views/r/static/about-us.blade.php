bd7063d1eb7ff2b0fc3f03866616e92aa8e5ab33
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-18 17:22:44 -0400

    updated about page

--- resources/views/r/static/about-us.blade.php
+++ resources/views/r/static/about-us.blade.php
@@ -62,14 +62,14 @@
 							<span>Chief Copywriter</span>
 						</figcaption>
 					</figure>
-					<ul class="list-inline">
+					<!-- <ul class="list-inline">
 						<li>
 							<a href="https://www.linkedin.com/in/ericanjanga/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
 						</li>
 						<li>
 							<a href="https://www.instagram.com/ericnjanga/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
 						</li>
-					</ul>
+					</ul> -->
 					<p>Born and raised up In Douala (Cameron), she is a very brilliant, talented and dynamic lady. Always very active when it comes to work.  She is the chief copywriter of this  TV brand. Academically, she is very studious and successfully obtained a masters degree In entrepreneuriat communication from the university of Douala (Cameron). She is a journalist, TV show  host and also do present events too. She loves reading, writing and travelling. She loves to associate herself with hard working people, those who are constantly involve in creating innovative stuffs, people who are in search of originality. She is a volunteer in so many projets, she fights so hard when it comes to realising her dreams. Her greatest dream is to own a purely entertainment TV station. She lives in Douala ( Cameron) and hopes to explore other nations with time.</p>
 				</article>
 
