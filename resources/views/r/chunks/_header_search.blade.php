de219cc8f0c0d1bd0deb6d7a993cceee5ec4a8d1
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-20 11:08:30 -0400

    improving translation

--- resources/views/r/chunks/_header_search.blade.php
+++ resources/views/r/chunks/_header_search.blade.php
@@ -9,7 +9,7 @@
 
 
 
-
+To activate a plan, start by picking a p
 
 <header class="ukb-main-header">
 	<nav class="navbar navbar-default">
@@ -17,7 +17,7 @@
 	    <!-- Brand and toggle -->
 	    <div class="navbar-header">
 	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-	        <span class="sr-only">Toggle navigation</span>
+	        <span class="sr-only">{{trans('messages.toggle_navigation')}}</span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
--- resources/views/r/layouts/user-search.blade.php
+++ resources/views/r/layouts/user-search.blade.php
@@ -1,7 +1,7 @@
 @extends('r.layouts.app')
 @section('layout')
     @include('r.chunks._header_search')
-    <main>
+    <main style="border:10px solid blue!important;">
         @yield('content')
     </main>
     @include('r.chunks._footer_main')
