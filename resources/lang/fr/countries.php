7d386d42edcd435ac946363f0198bcefe163b110
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-22 11:45:22 -0400

    upated country list

--- resources/lang/fr/countries.php
+++ resources/lang/fr/countries.php
@@ -1,10 +1,61 @@
 <?php
 
 return [
-
-    'countries' => [
-        'Algeria',
-        'Angola'
-    ]
-
+  'countries' => [
+    // 'Algeria',
+    // 'Angola',
+    // 'Benin',
+    // 'Botswana',
+    // 'Burkina Faso',
+    // 'Burundi',
+    // 'Cabo Verde',
+    'Cameroon',
+    // 'Central African Republic (CAR)',
+    // 'Chad',
+    // 'Comoros',
+    // 'Democratic Republic of the Congo',
+    // 'Republic of the Congo',
+    'Cote d\'Ivoire',
+    // 'Djibouti',
+    // 'Egypt',
+    // 'Equatorial Guinea',
+    // 'Eritrea',
+    'Ethiopia',
+    'France',
+    'Gabon',
+    // 'Gambia',
+    'Ghana',
+    'Guinea',
+    // 'Guinea-Bissau',
+    'Kenya',
+    // 'Lesotho',
+    // 'Liberia',
+    // 'Libya',
+    // 'Madagascar',
+    // 'Malawi',
+    // 'Mali',
+    // 'Mauritania',
+    // 'Mauritius',
+    // 'Morocco',
+    // 'Mozambique',
+    // 'Namibia',
+    // 'Niger',
+    // 'Nigeria',
+    // 'Rwanda',
+    // 'Sao Tome and Principe',
+    // 'Senegal',
+    // 'Seychelles',
+    // 'Sierra Leone',
+    // 'Somalia',
+    // 'South Africa',
+    // 'South Sudan',
+    // 'Sudan',
+    // 'Swaziland',
+    // 'Tanzania',
+    // 'Togo',
+    // 'Tunisia',
+    // 'Uganda',
+    // 'Zambia',
+    // 'Zimbabwe'
+  ]
 ];
