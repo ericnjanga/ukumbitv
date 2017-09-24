0e1f1d89b96174490a420558c77cb2722850dcd9
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-24 09:56:02 -0400

    adding video badges

--- resources/views/r/chunks/_video_item.blade.php
+++ resources/views/r/chunks/_video_item.blade.php
@@ -1,10 +1,10 @@
-<figure class="video-item @if($video->created_at->between(\Carbon\Carbon::now(), \Carbon\Carbon::now()->subDays(2))) new @endif">
+<figure class="video-item ruby-hover @if($video->created_at->between(\Carbon\Carbon::now(), \Carbon\Carbon::now()->subDays(2))) new @endif">
   <a href="{{route('user.singleVideo',$video->watchid)}}">
   	<span class="badge video-item__badge">{{trans('messages.new')}}</span>
-
-    <div class="video-item__img">
-      <img data-src="{{$video->videoimage->imgPreview1}}" class="lazyload" alt="{{$video->title}}">
-    </div>
+		<img data-src="{{$video->videoimage->imgPreview1}}" class="lazyload video-item__img" alt="{{$video->title}}">
+    <!-- <span class="video-item__img" style="display:block;">
+      
+    </span> -->
     <figcaption class="video-item__title ellipsis-gradient">{{$video->title}}
     </figcaption>
   </a>
