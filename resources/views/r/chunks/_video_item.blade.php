e96267f01ff8fa16d20573259b98d09334b81dd7
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-24 06:09:16 -0400

    adding video badges

--- resources/views/r/chunks/_video_item.blade.php
+++ resources/views/r/chunks/_video_item.blade.php
@@ -2,13 +2,13 @@
   <a href="{{route('user.singleVideo',$video->watchid)}}">
   	<span class="badge video-item__badge">{{trans('messages.new')}}</span>
 
-    <div class="video-img">
+    <div class="video-item__img">
       <img data-src="{{$video->videoimage->imgPreview1}}" class="lazyload" alt="{{$video->title}}">
     </div>
-    <figcaption class="video-title ellipsis-gradient">{{$video->title}}
+    <figcaption class="video-item__title ellipsis-gradient">{{$video->title}}
     </figcaption>
   </a>
-  <div class="video-info">
+  <div class="video-item__info">
     <div class="video-genre">{{$video->category->name}}</div>
     <div class="butn-like"><span class="icon icon-thumbs-up"></span><span class="likes-count">{{count($video->likes)}}</span></div>
   </div>
