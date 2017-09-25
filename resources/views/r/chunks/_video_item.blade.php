0304b38357ceeeabc69c7c8b9d28110d57195d96
Alex T <tgnwest@gmail.com>
2017-09-25 15:07:07 +0300

    add substr for video title

--- resources/views/r/chunks/_video_item.blade.php
+++ resources/views/r/chunks/_video_item.blade.php
@@ -1,11 +1,11 @@
 <figure class="video-item ruby-hover @if($video->created_at->between(\Carbon\Carbon::now(), \Carbon\Carbon::now()->subDays(2))) new @endif">
   <a href="{{route('user.singleVideo',$video->watchid)}}" class="video-item__frame">
   	<span class="badge video-item__badge">{{trans('messages.new')}}</span>
-  	<span class="video-item__resume">{{$video->description}}</span>
+  	<span class="video-item__resume">{{substr($video->description , 0,50)}}</span>
 		<img data-src="{{$video->videoimage->imgPreview1}}" class="lazyload video-item__img" alt="{{$video->title}}"> 
   </a>
   
-  <figcaption class="video-item__title ellipsis-gradient">{{substr($video->title , 0,50)}}
+  <figcaption class="video-item__title ellipsis-gradient">{{$video->title}}
   </figcaption>
   <div class="video-item__info">
     <div class="video-genre">{{$video->category->name}}</div>
