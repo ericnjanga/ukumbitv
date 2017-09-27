1320aeaa90842948dfa541b7cd02d5f818d3386d
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-27 16:57:55 -0400

    updating main search

--- resources/views/static/masterpage-404.blade.php
+++ resources/views/static/masterpage-404.blade.php
@@ -1,6 +1,6 @@
 @extends('r.layouts.app')
 @section('layout')
-    @include('r.chunks._header_search')
+    @include('r.chunks._header_logo_only')
     <main>
         @yield('content')
     </main>
