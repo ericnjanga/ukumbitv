a687e2a8bb4b36ecd17a6ec21c8c1f0ff9499301
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-27 17:10:17 -0400

    updating main search

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,8 +6,16 @@
 
 @section('content')
 
-  <div class="container-fluid page-404">
-  	<h1 class="text-center">Page Not found</h1>
+  <div class="panel panel-default page-404">
+  	<h1>La page n'a pas été trouvée</h1>
+		<p>La page demandée n'existe pas. Nous tenterons de vous rediriger automatiquement vers notre page d'accueil dans 10 secondes.</p>
+		<p>Veuillez aller à la page d'accueil de Netflix en cliquant sur le bouton ci-dessous.</p>
+
+		<div>
+			<a href="/" class="btn btn-cta1b btn-lg">UkumbiTV Home</a> 
+		</div>
+
+
   </div><!--end off canvas wrapper-->
     
 
