2f21aa03618ac18a479ce24df479eb8c39c4db2b
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-26 21:11:24 -0400

    cleanup

--- resources/views/r/user/single-video.blade.php
+++ resources/views/r/user/single-video.blade.php
@@ -62,9 +62,6 @@
 					</div> -->
 
 
-					<iframe src="//player.vimeo.com/hubnut/album/4787078?color=44bbff&amp;background=000000&amp;slideshow=0&amp;video_title=1&amp;video_byline=1" width="400" height="300" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-
-
 
 					
 
