049d2ca0d2e342d91e7ba46e9a4321439b3d6f3a
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-27 17:21:03 -0400

    updating 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,8 +8,10 @@
 
   <div class="panel panel-default page-404">
   	<h1>La page n'a pas été trouvée</h1>
-		<p>La page demandée n'existe pas. Nous tenterons de vous rediriger automatiquement vers notre page d'accueil dans 10 secondes.</p>
+		<p>La page demandée n'existe pas. Nous tenterons de vous rediriger automatiquement vers notre page d'accueil dans <span></span> <span id="countdown" style="font-weight:bold;">10</span> secondes</span>.</p>
 		<p>Veuillez aller à la page d'accueil de Netflix en cliquant sur le bouton ci-dessous.</p>
+
+		<br>
 
 		<div>
 			<a href="/" class="btn btn-cta1b btn-lg">UkumbiTV Home</a> 
@@ -17,6 +19,18 @@
 
 
   </div><!--end off canvas wrapper-->
+
+  <script>
+  	//Redirect to home page after "timeleft" seconds
+	  var timeleft = 10;
+		var downloadTimer = setInterval(function(){
+		  document.getElementById('countdown').value = 10 - --timeleft;
+		  if(timeleft <= 0){
+		  	document.location.href = document.location.origin;
+		  	clearInterval(downloadTimer);
+		  }  
+		},1000);
+  </script>
     
 
 @endsection
