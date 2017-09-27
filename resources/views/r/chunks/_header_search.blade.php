95070e3f0933fbe49403462951bf31ac52bf00d1
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-27 16:27:50 -0400

    updating main search

--- resources/views/r/chunks/_header_search.blade.php
+++ resources/views/r/chunks/_header_search.blade.php
@@ -46,6 +46,55 @@
 
 
 
+	    	<!-- <ul class="ukb-nav-videos nav navbar-nav">
+	        <li id="movies">
+						<a href="{{route('user.videotype', 'movies')}}"> 
+							{{trans('messages.movies')}}
+						</a> 
+					</li>
+					<li id="animations">
+						<a href="{{route('user.videotype', 'animations')}}"> 
+							{{trans('messages.animations')}}
+						</a> 
+					</li>  
+	      </ul> -->
+
+
+
+
+				
+
+	      <!-- <div id="frame-search" class="frame-search">
+			  
+	        <form action="{{route('search-all')}}" class="navbar-form navbar-left" method="post">  
+	          <div class="input-group">
+				      <input name="key" type="text" id="search-input" class="form-control search-input typeahead" placeholder="{{trans('messages.Search_placeholder')}}" autocomplete="false">
+				      <span class="input-group-btn">
+				        <button class="btn btn-search btn-primary" type="submit"> 
+				      		<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+				      		<span class="sr-only">{{trans('messages.search')}}!</span>
+				      	</button>
+				      </span>
+				    </div>
+	        </form> 
+	      </div> -->
+
+	      <ul class="nav navbar-nav navbar-right"> 
+	        <li class="dropdown"> 
+	          @include('r.chunks._login_block') 
+	        </li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+</header>
+
+
+
+
+
+
+
 
 			<div id="morphsearch" class="morphsearch">
 				<form class="morphsearch-form">
@@ -139,48 +188,3 @@
 			</div><!-- /morphsearch -->
 			 
 			<div class="ukumbitv-overlay"></div>
-
-
-			
-	    	<!-- <ul class="ukb-nav-videos nav navbar-nav">
-	        <li id="movies">
-						<a href="{{route('user.videotype', 'movies')}}"> 
-							{{trans('messages.movies')}}
-						</a> 
-					</li>
-					<li id="animations">
-						<a href="{{route('user.videotype', 'animations')}}"> 
-							{{trans('messages.animations')}}
-						</a> 
-					</li>  
-	      </ul> -->
-
-
-
-
-				
-
-	      <!-- <div id="frame-search" class="frame-search">
-			  
-	        <form action="{{route('search-all')}}" class="navbar-form navbar-left" method="post">  
-	          <div class="input-group">
-				      <input name="key" type="text" id="search-input" class="form-control search-input typeahead" placeholder="{{trans('messages.Search_placeholder')}}" autocomplete="false">
-				      <span class="input-group-btn">
-				        <button class="btn btn-search btn-primary" type="submit"> 
-				      		<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-				      		<span class="sr-only">{{trans('messages.search')}}!</span>
-				      	</button>
-				      </span>
-				    </div>
-	        </form> 
-	      </div> -->
-
-	      <ul class="nav navbar-nav navbar-right"> 
-	        <li class="dropdown"> 
-	          @include('r.chunks._login_block') 
-	        </li>
-	      </ul>
-	    </div><!-- /.navbar-collapse -->
-	  </div><!-- /.container-fluid -->
-	</nav>
-</header>
