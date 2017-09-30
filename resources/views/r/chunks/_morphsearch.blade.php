0e27135873a5824e0953d8d5747558fba68a993e
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-09-30 06:40:09 -0400

    Improving search system

--- resources/views/r/chunks/_morphsearch.blade.php
+++ resources/views/r/chunks/_morphsearch.blade.php
@@ -35,54 +35,54 @@
 			<div class="dummy-column">
 				<h2>Popular</h2>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/08/05/page-preloading-effect/">
-					<img src="img/thumbs/PagePreloadingEffect.png" alt="PagePreloadingEffect"/>
+					<!-- <img src="img/thumbs/PagePreloadingEffect.png" alt="PagePreloadingEffect"/> -->
 					<h3>Page Preloading Effect</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/05/28/arrow-navigation-styles/">
-					<img src="img/thumbs/ArrowNavigationStyles.png" alt="ArrowNavigationStyles"/>
+					<!-- <img src="img/thumbs/ArrowNavigationStyles.png" alt="ArrowNavigationStyles"/> -->
 					<h3>Arrow Navigation Styles</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/06/19/ideas-for-subtle-hover-effects/">
-					<img src="img/thumbs/HoverEffectsIdeasNew.png" alt="HoverEffectsIdeasNew"/>
+					<!-- <img src="img/thumbs/HoverEffectsIdeasNew.png" alt="HoverEffectsIdeasNew"/> -->
 					<h3>Ideas for Subtle Hover Effects</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/07/14/freebie-halcyon-days-one-page-website-template/">
-					<img src="img/thumbs/FreebieHalcyonDays.png" alt="FreebieHalcyonDays"/>
+					<!-- <img src="img/thumbs/FreebieHalcyonDays.png" alt="FreebieHalcyonDays"/> -->
 					<h3>Halcyon Days Template</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/05/22/inspiration-for-article-intro-effects/">
-					<img src="img/thumbs/ArticleIntroEffects.png" alt="ArticleIntroEffects"/>
+					<!-- <img src="img/thumbs/ArticleIntroEffects.png" alt="ArticleIntroEffects"/> -->
 					<h3>Inspiration for Article Intro Effects</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/06/26/draggable-dual-view-slideshow/">
-					<img src="img/thumbs/DraggableDualViewSlideshow.png" alt="DraggableDualViewSlideshow"/>
+					<!-- <img src="img/thumbs/DraggableDualViewSlideshow.png" alt="DraggableDualViewSlideshow"/> -->
 					<h3>Draggable Dual-View Slideshow</h3>
 				</a>
 			</div>
 			<div class="dummy-column">
 				<h2>Recent</h2>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/10/07/tooltip-styles-inspiration/">
-					<img src="img/thumbs/TooltipStylesInspiration.png" alt="TooltipStylesInspiration"/>
+					<!-- <img src="img/thumbs/TooltipStylesInspiration.png" alt="TooltipStylesInspiration"/> -->
 					<h3>Tooltip Styles Inspiration</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/09/23/animated-background-headers/">
-					<img src="img/thumbs/AnimatedHeaderBackgrounds.png" alt="AnimatedHeaderBackgrounds"/>
+					<!-- <img src="img/thumbs/AnimatedHeaderBackgrounds.png" alt="AnimatedHeaderBackgrounds"/> -->
 					<h3>Animated Background Headers</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/09/16/off-canvas-menu-effects/">
-					<img src="img/thumbs/OffCanvas.png" alt="OffCanvas"/>
+					<!-- <img src="img/thumbs/OffCanvas.png" alt="OffCanvas"/> -->
 					<h3>Off-Canvas Menu Effects</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/09/02/tab-styles-inspiration/">
-					<img src="img/thumbs/TabStyles.png" alt="TabStyles"/>
+					<!-- <img src="img/thumbs/TabStyles.png" alt="TabStyles"/> -->
 					<h3>Tab Styles Inspiration</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/08/19/making-svgs-responsive-with-css/">
-					<img src="img/thumbs/ResponsiveSVGs.png" alt="ResponsiveSVGs"/>
+					<!-- <img src="img/thumbs/ResponsiveSVGs.png" alt="ResponsiveSVGs"/> -->
 					<h3>Make SVGs Responsive with CSS</h3>
 				</a>
 				<a class="dummy-media-object" href="http://tympanus.net/codrops/2014/07/23/notification-styles-inspiration/">
-					<img src="img/thumbs/NotificationStyles.png" alt="NotificationStyles"/>
+					<!-- <img src="img/thumbs/NotificationStyles.png" alt="NotificationStyles"/> -->
 					<h3>Notification Styles Inspiration</h3>
 				</a>
 			</div>
