32ce5239ac57075bc704a0bb1818e868b9bcddb0
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-02 10:40:08 -0400

    improving search

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ elixir(function(mix) {
 		'main.js', 
 		'forms-validation.js',
 		'morphSearch.js'], 
-	'public/js/app335.js')
+	'public/js/app336.js')
 });
 
 
--- resources/views/r/layouts/app.blade.php
+++ resources/views/r/layouts/app.blade.php
@@ -135,7 +135,7 @@
 -->
 
 	<!-- Main JS file -->
-	<script src="{{asset('js/app335.js')}}"></script>  
+	<script src="{{asset('js/app336.js')}}"></script>  
 
 	@yield('scripts')
 
