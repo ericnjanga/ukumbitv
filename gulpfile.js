02d8a3bd7ee6796c6e05fcbc2e7de19a9e9ec935
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-02 10:34:31 -0400

    improving search

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ elixir(function(mix) {
 	//Compile all "scss" files into a "style.css" file
 	mix.sass([
         'resources/assets/sass/style.scss' 
-    ], 'public/r/css/style.css');
+    ], 'public/r/css/style0324.css');
 
     
 	//Concatenating local librairies and main js files 
