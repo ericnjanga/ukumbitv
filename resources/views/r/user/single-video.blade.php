d889f711e740b7cea95a61d43337294ae88ce6a3
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-02 12:42:12 -0400

    improving episodes system

--- resources/views/r/user/single-video.blade.php
+++ resources/views/r/user/single-video.blade.php
@@ -85,7 +85,7 @@
 								  <!-- <label for="video-episodes"></label> -->
 								  <select name="video-episodes" id="video-episodes" class="form-control">
 									  @foreach($episodesArr as $indexKey => $episode)
-										  <option data-title="{{$episode.title}}" value="{{$episode}}">Episode {{++$indexKey}} ({{$episode.title}})</option>
+										  <option data-title="{{$episode->title}}" value="{{$episode}}">Episode {{++$indexKey}} ({{$episode->title}})</option>
 									  @endforeach
 								  </select>
 							  </div>
