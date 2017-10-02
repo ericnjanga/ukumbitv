862599c028ba2b11003f70644c1b76054c33ebd2
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-02 13:13:56 -0400

    improving main header

--- resources/views/r/chunks/_login_block.blade.php
+++ resources/views/r/chunks/_login_block.blade.php
@@ -3,36 +3,37 @@
 
 	<a href="#" class="dropdown-toggle nav-user-tmb" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
 		{{--<i class="fa fa-user-circle-o user-tmb" aria-hidden="true"></i>--}}
-		 <img class="img-circle img-responsive" style="width: 30px;height: 30px;" src="{{Auth::user()->picture}}" alt="">
-		<span class="caret"></span>
-	</a>
+		<img class="img-circle img-responsive" src="{{Auth::user()->picture}}" alt="">
 
-	<span class="nav-package-info">
-    <span id="link-update-package" data-route="{{route('user.package')}}">
-			@if(Auth::user()->paymentPlans[0]->flag != 3)
-				{{trans('messages.msg_upgrade')}}
-			@else
-			@endif
-    </span> 
-    <span class="login-text">
-			<span class="bold">
-				{{--username--}}
-				@if(Auth::user()->name !== '')
-					{{Auth::user()->name}}
+		<span class="nav-package-info">
+	    <span id="link-update-package" data-route="{{route('user.package')}}">
+				@if(Auth::user()->paymentPlans[0]->flag != 3)
+					{{trans('messages.msg_upgrade')}}
 				@else
-					{{Auth::user()->email}}
 				@endif
-				{{--end username--}}
-			</span>
-			<span class="login-text-vidcounter">
-				@if(isset($videoCount))
-	    		{{$videoCount}} {{trans('messages.videos_left')}}
-	    	@else 
-	    		{{trans('messages.unlimited_videos')}}
-	    	@endif
-			</span>
-    </span>
-	</span>   
+	    </span> 
+	    <span class="login-text">
+				<span class="bold">
+					{{--username--}}
+					@if(Auth::user()->name !== '')
+						{{Auth::user()->name}}
+					@else
+						{{Auth::user()->email}}
+					@endif
+					{{--end username--}}
+				</span>
+				<span class="login-text-vidcounter">
+					@if(isset($videoCount))
+		    		{{$videoCount}} {{trans('messages.videos_left')}}
+		    	@else 
+		    		{{trans('messages.unlimited_videos')}}
+		    	@endif
+				</span>
+	    </span>
+		</span>  
+		
+		<span class="caret"></span>
+	</a> 
 
 	<ul class="dropdown-menu">
 		<li><a href="{{route('user.account')}}">{{trans('messages.my_account')}}</a></li>
