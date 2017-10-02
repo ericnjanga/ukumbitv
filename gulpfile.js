4e0cd113c2369f1f928c063c75aab3e1481b964c
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-02 14:06:37 -0400

    improving search

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ elixir(function(mix) {
 	//Compile all "scss" files into a "style.css" file
 	mix.sass([
         'resources/assets/sass/style.scss' 
-    ], 'public/r/css/style0353.css');
+    ], 'public/r/css/style0354.css');
 
     
 	//Concatenating local librairies and main js files 
@@ -37,7 +37,7 @@ elixir(function(mix) {
 		'main.js', 
 		'forms-validation.js',
 		'morphSearch.js'], 
-	'public/js/app0353.js')
+	'public/js/app0354.js')
 });
 
 
