68395816fdcf1096336662fcd814b0956d1d78f1
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-03 08:15:26 -0400

    improving contact email

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -4,21 +4,21 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>{{Setting::get('site_name')}}</title>
+  <title>Message from CONTACT FORM</title>
 </head>
 <body>
-  <h1 style="color:#000;"><span style="color:#666;">MESSAGE FROM</span> CONTACT FORM</h1>
+  <h1 style="color:#000;"><span style="color:#666;">Message from</span> CONTACT FORM</h1>
   <table border="0" width="100%">
   	<tr>
-  		<td style="color:#666;" width="120px; padding: 10px;">Category:</td>
+  		<td style="color:#666; font-weight:bold;" width="120px; padding: 10px;">Category:</td>
   		<td style="color:#000; padding: 10px;">{{$email_data->category}}</td>
   	</tr>
   	<tr>
-  		<td style="color:#666;" width="120px; padding: 10px;">Email from:</td>
+  		<td style="color:#666; font-weight:bold;" width="120px; padding: 10px;">Email from:</td>
   		<td style="color:#000; padding: 10px;">{{$email_data->email}}</td>
   	</tr>
   	<tr>
-  		<td style="color:#666;" width="120px; padding: 10px;">Message:</td>
+  		<td style="color:#666; font-weight:bold;" width="120px; padding: 10px;">Message:</td>
   		<td style="color:#000; padding: 10px;">{{$email_data->message}}</td>
   	</tr>
   </table>
