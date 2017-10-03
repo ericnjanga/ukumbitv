f8bf8d8e73ff59f9d5e9ec21023928dc8fb9b1a8
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-03 08:12:27 -0400

    improving contact email

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -2,126 +2,25 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>{{Setting::get('site_name')}}</title>
-    <style type="text/css">
-
-        body {
-            background-color: black;
-        }
-
-        #outlook a {padding:0;}
-        body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
-        .ExternalClass {width:100%;}
-        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;}
-        #backgroundTable {margin:0; padding:0; width:100% !important; line-height: 100% !important;}
-        img {outline:none; text-decoration:none; -ms-interpolation-mode: bicubic;}
-        a img {border:none;display:inline-block;}
-        .image_fix {display:block;}
-
-        h1, h2, h3, h4, h5, h6 {color: white !important;}
-
-        h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {color: blue !important;}
-
-        h1 a:active, h2 a:active,  h3 a:active, h4 a:active, h5 a:active, h6 a:active {
-            color: red !important;
-        }
-
-        h1 a:visited, h2 a:visited,  h3 a:visited, h4 a:visited, h5 a:visited, h6 a:visited {
-            color: purple !important;
-        }
-
-        table td {border-collapse: collapse;}
-
-        table { border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
-
-        a {color: #000;}
-
-        @media only screen and (max-device-width: 480px) {
-
-            a[href^="tel"], a[href^="sms"] {
-                text-decoration: none;
-                color: black; /* or whatever your want */
-                pointer-events: none;
-                cursor: default;
-            }
-
-            .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
-                text-decoration: default;
-                color: orange !important; /* or whatever your want */
-                pointer-events: auto;
-                cursor: default;
-            }
-        }
-
-
-        @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
-            a[href^="tel"], a[href^="sms"] {
-                text-decoration: none;
-                color: blue; /* or whatever your want */
-                pointer-events: none;
-                cursor: default;
-            }
-
-            .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
-                text-decoration: default;
-                color: orange !important;
-                pointer-events: auto;
-                cursor: default;
-            }
-        }
-
-        p {
-            margin:0;
-            color:#FFF;
-            font-family:Helvetica, Arial, sans-serif;
-            font-size:16px;
-            line-height:160%;
-        }
-        a.link2{
-            text-decoration:none;
-            font-family:Helvetica, Arial, sans-serif;
-            font-size:16px;
-            color:#fff;
-            border-radius:4px;
-        }
-        h2{
-            color:#FFF;
-            font-family:Helvetica, Arial, sans-serif;
-            font-size:22px;
-            font-weight: normal;
-        }
-
-        .bgItem{
-            background:#cf4545;
-        }
-        .bgBody{
-            background:#ffffff;
-        }
-
-        .table-full{
-            height:100%;
-        }
-
-    </style>
-
-    <script type="colorScheme" class="swatch active">
-  {
-    "name":"Default",
-    "bgBody":"ffffff",
-    "link":"f2f2f2",
-    "color":"555555",
-    "bgItem":"F4A81C",
-    "title":"181818"
-  }
-</script>
-
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>{{Setting::get('site_name')}}</title>
 </head>
-<body >
-    <h1>CONTACT FORM</h1>
-    <p>Category: {{$email_data->category}}</p>
-    <p>Email from: {{$email_data->email}}</p>
-    <p>Message: {{$email_data->message}}</p>
+<body>
+  <h1 style="color:#000;"><span style="color:#666;">MESSAGE FROM</span> CONTACT FORM</h1>
+  <table border="0" width="100%">
+  	<tr>
+  		<td style="color:#666;" width="120px; padding: 10px;">Category:</td>
+  		<td style="color:#000; padding: 10px;">{{$email_data->category}}</td>
+  	</tr>
+  	<tr>
+  		<td style="color:#666;" width="120px; padding: 10px;">Email from:</td>
+  		<td style="color:#000; padding: 10px;">{{$email_data->email}}</td>
+  	</tr>
+  	<tr>
+  		<td style="color:#666;" width="120px; padding: 10px;">Message:</td>
+  		<td style="color:#000; padding: 10px;">{{$email_data->message}}</td>
+  	</tr>
+  </table>
 </body>
 </html>
