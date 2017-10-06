61c272d21909e32c8877a8f6adde3aa06c968920
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-06 08:23:07 -0400

    changing alert message

--- resources/views/r/chunks/_header_search.blade.php
+++ resources/views/r/chunks/_header_search.blade.php
@@ -1,13 +1,4 @@
-<!-- email confirmation reminder -->
-@if(Auth::check())
-@if(!Auth::user()->isVerified())
-	<div class="alert__force-notice alert alert-info text-center" role="alert">
-		{{trans('messages.auth_confirm_reminder1')}}
-		<a href="{{route('user.confirm-user-email')}}"><b>{{trans('messages.auth_confirm_reminder2')}}</b></a> <i class="fa fa-smile-o" aria-hidden="true"></i>
-	</div>
-@endif
-@endif
-<!-- email confirmation reminder -->
+
 
 
 
--- resources/views/r/layouts/user-search.blade.php
+++ resources/views/r/layouts/user-search.blade.php
@@ -1,9 +1,26 @@
 @extends('r.layouts.app')
 @section('layout')
-    @include('r.chunks._morphsearch')
-    @include('r.chunks._header_search')
-    <main>
-        @yield('content')
-    </main>
-    @include('r.chunks._footer_main')
+  @include('r.chunks._morphsearch')
+  @include('r.chunks._header_search')
+  <main>
+      @yield('content')
+  </main>
+  @include('r.chunks._footer_main')
+@endsection
+
+
+@section('scripts')
+<!-- email confirmation reminder -->
+@if(Auth::check())
+@if(!Auth::user()->isVerified())
+<script>
+	swal ( "Oops" ,  "Something went wrong!" ,  "info" )
+</script>
+	<!-- <div class="alert__force-notice alert alert-info text-center" role="alert">
+		{{trans('messages.auth_confirm_reminder1')}}
+		<a href="{{route('user.confirm-user-email')}}"><b>{{trans('messages.auth_confirm_reminder2')}}</b></a> <i class="fa fa-smile-o" aria-hidden="true"></i>
+	</div> -->
+@endif
+@endif
+<!-- email confirmation reminder -->
 @endsection
