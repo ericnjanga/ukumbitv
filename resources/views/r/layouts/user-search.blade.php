27bbdcdf82b6370101b669deb0529a1a00786940
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-06 08:37:35 -0400

    changing alert message

--- resources/views/r/layouts/user-search.blade.php
+++ resources/views/r/layouts/user-search.blade.php
@@ -2,25 +2,40 @@
 @section('layout')
   @include('r.chunks._morphsearch')
   @include('r.chunks._header_search')
-  <main style="border:25px solid lime!important;">
-      @yield('content')
+  <main>
+
+
+
+  	@if(Auth::check())
+		@if(!Auth::user()->isVerified())
+		<h1>cl;dsdskvs ??????</h1>
+		<h1>cl;dsdskvs ??????</h1>
+		<h1>cl;dsdskvs ??????</h1>
+		<h1>cl;dsdskvs ??????</h1>
+		<h1>cl;dsdskvs ??????</h1>
+		@endif
+	@endif
+
+
+
+    @yield('content')
   </main>
   @include('r.chunks._footer_main')
 @endsection
 
 
 @section('scripts')
-<!-- email confirmation reminder -->
-@if(Auth::check())
-@if(!Auth::user()->isVerified())
-<script>
-	swal ( "Oops" ,  "Something went wrong!" ,  "info" )
-</script>
-	<!-- <div class="alert__force-notice alert alert-info text-center" role="alert">
-		{{trans('messages.auth_confirm_reminder1')}}
-		<a href="{{route('user.confirm-user-email')}}"><b>{{trans('messages.auth_confirm_reminder2')}}</b></a> <i class="fa fa-smile-o" aria-hidden="true"></i>
-	</div> -->
-@endif
-@endif
-<!-- email confirmation reminder -->
+	<!-- email confirmation reminder -->
+	@if(Auth::check())
+		@if(!Auth::user()->isVerified())
+		<script>
+			swal ( "Oops" ,  "Something went wrong!" ,  "info" )
+		</script>
+			<!-- <div class="alert__force-notice alert alert-info text-center" role="alert">
+				{{trans('messages.auth_confirm_reminder1')}}
+				<a href="{{route('user.confirm-user-email')}}"><b>{{trans('messages.auth_confirm_reminder2')}}</b></a> <i class="fa fa-smile-o" aria-hidden="true"></i>
+			</div> -->
+		@endif
+	@endif
+	<!-- email confirmation reminder -->
 @endsection
