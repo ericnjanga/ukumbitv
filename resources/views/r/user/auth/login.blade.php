a5149b79ce444ce007691ffa2bc46a4b16d67595
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-19 12:01:10 -0400

    improving login/registrations UX

--- resources/views/r/user/auth/login.blade.php
+++ resources/views/r/user/auth/login.blade.php
@@ -20,7 +20,7 @@ page-authentication page-login
     <div class="panel-heading" role="tab" id="headingOne">
       <h4 class="panel-title">
         <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          {{trans('messages.auth_signin')}}
+          {{trans('messages.auth_signin_fb')}}
         </a>
       </h4>
     </div>
@@ -34,7 +34,7 @@ page-authentication page-login
 
 				@if(config('services.facebook.client_id') && config('services.facebook.client_secret'))
 			    <div class="text-center">
-			      <p>{{trans('messages.auth_signin_blurb')}}</p>
+			      <!-- <p>{{trans('messages.auth_signin_blurb')}}</p> -->
 			      <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
 			        <input type="hidden" value="facebook" name="provider" id="provider">
 			        {{--<a href="{{ route('SocialLogin') }}" class="btn btn-block btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>{{trans('messages.auth_signin_fb')}}</a>--}}
@@ -56,38 +56,14 @@ page-authentication page-login
     <div class="panel-heading" role="tab" id="headingTwo">
       <h4 class="panel-title">
         <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
+          Sign In with an email address
         </a>
       </h4>
     </div>
     <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-    <div class="or-line upper">{{trans('messages.auth_or')}}</div>
-		
-
-
-    <form name="loginForm" action="{{route('user.login.post')}}" method="POST" autocomplete="off" novalidate>
+      <div class="panel-body" style="padding: 20px;">
+        	
+        	<form name="loginForm" action="{{route('user.login.post')}}" method="POST" autocomplete="off" novalidate>
 
 			<!-- Email Address -->
       <div class="form-group" ng-class="{ 'has-error' : loginForm.email.$invalid && !loginForm.email.$pristine }">
@@ -120,6 +96,33 @@ page-authentication page-login
 
       <button type="submit" class="btn btn-block btn-cta1b btn-lg" ng-disabled="loginForm.$invalid">{{trans('messages.auth_signin')}}</button>
     </form>
+
+    
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    <div class="or-line upper">{{trans('messages.auth_or')}}</div>
+		
+
+
+    
 
     <div>
         <span>{{trans('messages.auth_no_account')}}</span>
