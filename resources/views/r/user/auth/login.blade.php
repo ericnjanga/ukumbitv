b46b0e246113af23b4304aa405e6c94ed0e591fb
Eric Njanga <ericnjanga@Erics-MacBook-Pro-2.local>
2017-10-25 09:37:05 -0400

    login change

--- resources/views/r/user/auth/login.blade.php
+++ resources/views/r/user/auth/login.blade.php
@@ -13,7 +13,7 @@ page-authentication page-login
 		.page {
 	    min-height: initial; 
 		}
-		#collapse-facebook .panel-body {
+		/*#collapse-facebook .panel-body {
 			min-height: 400px; 
 	    min-height: 400px;
 	    display: flex;
@@ -43,7 +43,7 @@ page-authentication page-login
 		  background-color: rgba(12, 90, 19, 0.7);
 		}
 
-		/*--- ---*/
+		 
 		.panel-heading .panel-title a.active {
 			color: #333!important;
 		  background-color: rgba(180, 180, 180, 0.6)!important;
@@ -74,11 +74,89 @@ page-authentication page-login
 		}
 		#log-accordion .panel {
 			box-shadow: 0 0 0 rgba(0,0,0,.05);
-		}
-
-		 
-
+		}*/ 
 	</style>
+
+
+
+
+
+
+
+	<div id="section-auth" class="section-auth"> 
+	  <!-- Nav tabs -->
+	  <ul class="nav nav-tabs" role="tablist">
+	    <li role="presentation" class="active"><a href="#tab-facebook" aria-controls="tab-facebook" role="tab" data-toggle="tab">Home</a></li>
+	    <li role="presentation"><a href="#tab-email" aria-controls="tab-email" role="tab" data-toggle="tab">Profile</a></li> 
+	  </ul>
+
+	  <!-- Tab panes -->
+	  <div class="tab-content">
+
+	    <div role="tabpanel" class="tab-pane active" id="tab-facebook">
+				@if(config('services.facebook.client_id') && config('services.facebook.client_secret'))
+			    <div class="text-center">
+			      <p>{{trans('messages.auth_signin_blurb')}}</p>
+			      <form class="social-form form-horizontal" role="form" method="POST" action="{{ route('SocialLogin') }}">
+			        <input type="hidden" value="facebook" name="provider" id="provider">
+			        {{--<a href="{{ route('SocialLogin') }}" class="btn btn-block btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>{{trans('messages.auth_signin_fb')}}</a>--}}
+			        <button type="submit" class="btn btn-block btn-lg butn-dblue butn-face"><span class="icon icon-facebook"></span>{{trans('messages.auth_signin_fb')}}</button>
+			      </form>
+			    </div>
+			  @endif
+	    </div><!-- tab-facebook -->
+
+	    <div role="tabpanel" class="tab-pane" id="tab-email"> 
+      	<form name="loginForm" action="{{route('user.login.post')}}" method="POST" autocomplete="off" novalidate>
+
+					<!-- Email Address -->
+		      <div class="form-group" ng-class="{ 'has-error' : loginForm.email.$invalid && !loginForm.email.$pristine }">
+		        <label for="email">{{trans('messages.auth_enter_email')}} <span>*</span></label>
+		        <input class="form-control" type="email" name="email" ng-model="user.email" required>
+		        <div ng-cloak ng-show="loginForm.email.$invalid && !loginForm.email.$pristine" class="help-block">{{trans('messages.auth_error_email')}}</div> 
+		      </div>
+
+
+		      <!-- Password -->
+		      <div class="form-group" ng-class="{ 'has-error' : loginForm.password.$invalid && !loginForm.password.$pristine }">
+		        <label>{{trans('messages.auth_enter_password')}}</label>   
+		        <input type="password" name="password" class="form-control" ng-model="user.password" ng-minlength="6" required>
+		        <div ng-cloak ng-show="loginForm.password.$dirty && loginForm.password.$viewValue.length==0" class="help-block">{{trans('messages.auth_error_password1')}}</div> 
+		        <div ng-cloak ng-show="loginForm.password.$error.minlength" class="help-block">{{trans('messages.auth_error_password2')}}</div>
+		        <div ng-cloak ng-show="loginForm.password.$error.pattern" class="help-block">{{trans('messages.auth_error_password3')}}</div>
+		    	</div>   
+ 
+		      <div class="checkbox">
+		      	<label for="remember">
+		      		<input id="remember" type="checkbox"> {{trans('messages.auth_remember')}}
+		      	</label>
+		        <div class="block-forgot-pass fine-print">
+		        	<a href="{{url('/password/reset')}}">{{trans('messages.auth_forgot_password')}}</a>
+		        </div> 
+		      </div>
+ 
+
+		      <button type="submit" class="btn btn-block btn-cta1b btn-lg" ng-disabled="loginForm.$invalid">{{trans('messages.auth_signin')}}</button>
+ 
+
+			    <div>
+			        <span>{{trans('messages.auth_no_account')}}</span>
+			        <a href="{{route('user.register.form')}}" class="sign-butn">{{trans('messages.auth_signup')}}</a>
+			    </div>
+		    </form><!-- loginForm -->
+	    </div><!-- tab-email -->
+
+	  </div><!-- tab-content -->
+	</div><!-- section-auth -->
+
+
+
+
+		<br>
+		<br>
+		<br>
+		<br>
+
  
   <div class="page page-auth" ng-app="validationApp" ng-controller="mainController" style="padding: 0;">
   	<div class="panel-group" id="log-accordion" role="tablist" aria-multiselectable="true">
